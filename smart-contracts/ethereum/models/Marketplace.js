36a0742e6ac88e4f6ab77a4384dc9ab933594f38
Eric Muyser <eric@muyser.com>
2018-04-08 16:34:27 -0700

    Update Marketplace model (call vs send)

--- smart-contracts/ethereum/models/Marketplace.js
+++ smart-contracts/ethereum/models/Marketplace.js
@@ -6,144 +6,82 @@ window.web3 = new window.Web3(new window.Web3.providers.HttpProvider("http://loc
 
 class Marketplace {
     constructor() {
-        this.apps = [
-            {
-                id: 'sadas3asdasd-asd34asd3-adsas3',
-                name: 'Battlerite',
-                type: 'game',
-                downloads: 4000,
-                owner: '0x0',
-                author: {
-                    id: '0x0'
-                },
-                republicTags: ['game', 'upcoming'],
-                authorTags: ['moba']
-            },
-            {
-                id: 'vsdvs-asd34asfdssd3-ad434sas3',
-                name: 'Fortnite',
-                type: 'game',
-                downloads: 4000,
-                owner: '0x0',
-                author: {
-                    id: '0x0'
-                },
-                republicTags: ['game', 'specials'],
-                authorTags: ['fps']
-            },
-            {
-                id: "29dsa90-asdsad9adas-asdadas-asdasd",
-                name: "World of Warcraft",
-                type: 'game',
-                downloads: 20,
-                republicTags: ['game', 'specials'],
-                authorTags: ['mmo']
-            },
-            {
-                id: "29dsa90-3434asd-asdadas-asdasd",
-                name: "Divvy",
-                type: 'app',
-                downloads: 0,
-                republicTags: ['productivity', 'specials'],
-                authorTags: ['hotkeys']
-            }
-        ]
     }
 
     init(contractMeta, contractAddress, fromAddress, toAddress) {
+        console.log("Initializing Marketplace contract", arguments)
+        
         //web3.setProvider(new web3.providers.HttpProvider("https://ropsten.infura.io/XXXXXX"))
         this.contractMeta = contractMeta
         this.contractAddress = contractAddress
         this.fromAddress = fromAddress
         this.toAddress = toAddress
         this.nonce = 0
+        this.contract = new web3.eth.Contract(this.contractMeta.abi, this.contractAddress)
     }
 
     async submitAppForReview(name, version, category, files, checksum, permissions) {
-        let contract = new web3.eth.Contract(this.contractMeta.abi, this.contractAddress)
-
         console.log('Calling Marketplace.submitAppForReview with arguments: ', arguments)
 
         return await new Promise((resolve) => {
-            web3.eth.getTransactionCount(this.fromAddress, (err, nonce) => {
-                let rawTransaction = {
-                    "from": this.fromAddress,
-                    "nonce": web3.utils.toHex(nonce),
-                    "gasPrice": web3.utils.toWei("0.000000021", "ether"),
-                    "gasLimit": web3.utils.toHex("900000"),
-                    "to": this.contractAddress,
-                    "value": "0x0",
-                    "data": contract.methods.submitAppForReview(name, web3.utils.asciiToHex(version), category, files, checksum, permissions).encodeABI(),
-                    "chainId": 0x03
-                }
-
-                web3.eth.sendTransaction(rawTransaction, (err, hash) => {
-                    if (err)
-                        console.log(err)
-
-                    console.log(hash)
+            this.contract.methods
+                .submitAppForReview(name, web3.utils.asciiToHex(version), category, files, checksum, permissions)
+                .send({ from: this.fromAddress, gas: 3000000 })
+                .on('receipt', (receipt) => {
+                    resolve(receipt.events.AppSubmitted.returnValues.id)
                 })
-
-                resolve()
-            })
         })
     }
 
     async voteForApp(id, version, vote) {
-        let contract = new web3.eth.Contract(this.contractMeta.abi, this.contractAddress)
-
         console.log('Calling Marketplace.voteForApp with arguments: ', arguments)
 
         return await new Promise((resolve) => {
-            web3.eth.getTransactionCount(this.fromAddress, (err, nonce) => {
-                let rawTransaction = {
-                    "from": this.fromAddress,
-                    "nonce": web3.utils.toHex(nonce),
-                    "gasPrice": "0x04e3b29200",
-                    "gasLimit": "0x7458",
-                    "to": this.contractAddress,
-                    "value": "0x0",
-                    "data": contract.methods.voteForApp(id, version, vote).encodeABI(),
-                    "chainId": 0x03
-                }
-
-                web3.eth.sendTransaction(rawTransaction, (err, hash) => {
-                    if (err)
-                        console.log(err)
-
-                    console.log(hash)
+            this.contract.methods
+                .voteForApp(id, web3.utils.asciiToHex(version), vote)
+                .send({ from: this.fromAddress, gas: 3000000 })
+                .on('receipt', (receipt) => {
+                    resolve()
                 })
-
-                resolve()
-            })
         })
     }
 
-    async getAppListing(page, offset) {
-        let contract = new web3.eth.Contract(this.contractMeta.abi, this.contractAddress)
-    
-        console.log(contract)
-        //abiDecoder.addABI(this.contractMeta.abi)
+    async updateAppVotes(id, version) {
+        console.log('Calling Marketplace.updateAppVotes with arguments: ', arguments)
 
-        console.log('Calling Marketplace.listApps with arguments: ', arguments)
         return await new Promise((resolve) => {
-            const data = contract.methods.listApps(page, offset).call({ from: this.fromAddress }, (err, res) => {
+            this.contract.methods
+                .updateAppVotes(id, web3.utils.asciiToHex(version))
+                .send({ from: this.fromAddress, gas: 3000000 })
+                .on('receipt', (receipt) => {
+                    resolve()
+                })
+        })
+    }
 
-                resolve(res)
-            })
+    async listApps(page, offset) {
+        console.log('Calling Marketplace.listApps with arguments: ', arguments)
+
+        return await new Promise((resolve) => {
+            const data = this.contract.methods
+                .listApps(page, offset)
+                .call({ from: this.fromAddress, gas: 3000000 }, (err, res) => {
+                    if (err) throw err
+                    resolve(res)
+                })
         })
     }
 
     async getApp(id) {
-        let contract = new web3.eth.Contract(this.contractMeta.abi, this.contractAddress)
-
         console.log('Calling Marketplace.getApp with arguments: ', arguments)
 
         return await new Promise((resolve) => {
-            const data = contract.methods.getLatestVersion(id).call({ from: this.fromAddress }, (err, res) => {
-
-                resolve(res)
-            })
+            const data = this.contract.methods
+                .getLatestVersion(id)
+                .call({ from: this.fromAddress, gas: 3000000 }, (err, res) => {
+                    if (err) throw err
+                    resolve(res)
+                })
         })
     }
 
