200915c0a6926c892e4802df7fc3166b9b19b1cd
Eric Muyser <eric@muyser.com>
2018-06-08 01:44:30 -0700

    Update
    Former-commit-id: 4f42a0f06f3e0c53eeea0ce07d4933637cadc5ca

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: yarn run ng serve -- --host 0.0.0.0 --port=$PORT
---
+++ src/app/theme/developer/developer-routing.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Developer',
+      status: false
+    },
+    children: [
+      {
+        path: 'signup',
+        loadChildren: './signup/signup.module#SignupModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DeveloperRoutingModule { }
