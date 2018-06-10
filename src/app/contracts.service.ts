0b5dcf9800deea9f29595cd7add7e7d16654e52f
Eric Muyser <eric@muyser.com>
2018-06-09 17:42:56 -0700

    Use random ID
    Former-commit-id: d22977acad4077c5c2ab9ea142027fab56503485

--- src/app/contracts.service.ts
+++ src/app/contracts.service.ts
@@ -8,6 +8,12 @@ declare let require: any;
 declare let window: any;
 
 let tokenAbi = require('./contracts/tokenContract.json');
+const ID = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 let raft = null
 let peer = null
