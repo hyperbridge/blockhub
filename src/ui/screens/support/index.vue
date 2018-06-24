73ca9fced0ce1ac2544e2bd9217928327815f63f
Eric Muyser <eric@muyser.com>
2018-06-23 21:25:24 -0700

    Support screen
    Former-commit-id: 1cca5d1dfec7c8d777a6511b0cb4fb6ba0f7e961

--- src/ui/screens/support/index.vue
+++ src/ui/screens/support/index.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>todo</div>
+    <c-layout navigationKey="helpNavigation">
+        <div class="content" id="content">
+            <div class="page-heading">
+                <div class="page-heading__container">
+                    <h1 class="title">Support</h1>
+                    <p class="caption">Let us know if you need further help.</p>
+                </div>
+                <c-heading />
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                </div>
+            </div>
+        </div>
+    </c-layout>
 </template>
 
 <script>
 export default {
   components: {
+    'c-layout': () => import('@/ui/layouts/default')
   },
   data: () => ({
-    wallets: []
   })
 }
 </script>
