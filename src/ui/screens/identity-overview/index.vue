8e7760a155ce6a190827223d831922d4592ad9e4
Eric Muyser <eric@muyser.com>
2018-06-26 23:50:50 -0700

    [WIP] Identity overview screen
    Former-commit-id: 853d1c57579e5966aa823068db241cb175d5ca56

--- src/ui/screens/identity-overview/index.vue
+++ src/ui/screens/identity-overview/index.vue
@@ -1,20 +1,279 @@
 <template>
     <c-layout>
+      <div class="content" id="content">
+        <div class="container-fluid">  
+                                    
       ID #{{id}}
+
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <div class="row">
+                                <div class="col-9">
+                                    
+                                    <div class="user user--bordered user--xlg margin-bottom-20">
+                                        <img src="http://via.placeholder.com/128x128">
+                                        <div class="user__name">
+                                            <strong>Tracey Newman</strong><br>
+                                            <span class="text-muted">UI/UX Designer</span>
+                                            <div class="rating_readonly" data-score="4"></div>    
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                <div class="col-3 text-right">
+                                    <button class="btn btn-light btn-icon"><span class="fa fa-pencil"></span></button>                                                
+                                </div>
+                            </div>                                        
+                            
+                            <p class="text-muted margin-bottom-20">I love my job and things i do, lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus sagittis volutpat. Ut id nisl at eros accumsan...</p>
+                            
+                            <div class="form-row">
+                                <div class="col-6">
+                                    <button class="btn btn-secondary btn-block btn--icon btn--icon-stacked"><span class="fa fa-envelope-o"></span> Message</button>
+                                </div>
+                                <div class="col-6">
+                                    <button class="btn btn-info btn-block btn--icon btn--icon-stacked btn--icon-right">Actions <span class="fa fa-ellipsis-v"></span></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="divider divider--dashed divider--sm"></div>
+                        <div class="card-body">
+                            
+                            <h5>Contact information</h5>
+                            <p>
+                                <span class="caption">Email adress:</span><br>
+                                tracey_newman@email.domain.com
+                            </p>
+                            <p>
+                                <span class="caption">Phone number:</span><br>
+                                +91987654321
+                            </p>
+                            <p>
+                                <span class="caption">Address:</span><br>
+                                West End 45th Street, San Francisco<br>
+                                90123 California, US
+                            </p>
+                        </div>
+                        <div class="divider divider--dashed divider--sm"></div>
+                        <div class="card-body">
+                            
+                            <h5>Custom information</h5>
+                            <p>
+                                <span class="caption">Facebook:</span><br>
+                                facebook.com/tracey89_girl
+                            </p>
+                            <p class="margin-bottom-30">
+                                <span class="caption">Twitter:</span><br>
+                                twitter.com/tracey_newman
+                            </p>                                            
+                            
+                            <h5>Team</h5>
+                            
+                            <div class="user-group user-group--bordered margin-bottom-10">
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>                                        
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>                                        
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>                                        
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>                                        
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>
+                                <div class="user user--lg user--rounded">
+                                    <img src="http://via.placeholder.com/128x128">
+                                </div>
+                            </div>
+                            <p class="text-muted text-sm">There are 7 persons in team of this member.</p>
+                            
+                        </div>
+                        <div class="divider divider--dashed divider--sm"></div>
+                        <div class="card-body">
+                            
+                            <h5>Tags</h5>
+                            
+                            <p>
+                                <span class="tag tag--transparent tag--block tag--sm">Tag name</span> 
+                                <span class="tag tag--transparent tag--block tag--sm">Another tag</span> 
+                                <span class="tag tag--transparent tag--block tag--sm">One more</span> 
+                                <span class="tag tag--transparent tag--block tag--sm">Custom</span> 
+                                <span class="tag tag--transparent tag--block tag--sm">Information</span>
+                                <span class="tag tag--transparent tag--block tag--sm">Phone</span>
+                                <span class="tag tag--transparent tag--block tag--sm">Email</span>
+                                <span class="tag tag--transparent tag--block tag--sm">UI/UX Designer</span>
+                            </p>
+                            
+                        </div>                                    
+                    </div>
+                    
+                </div>
+                <div class="col-12 col-lg-9">
+                    
+                    <div class="timeline">
+
+                        <div class="timeline__item">
+                            <div class="user"><img src="http://via.placeholder.com/128x128" alt="Taylor Watson"></div>
+                            <div class="content">
+                                <div class="title"><a href="#" class="text-info">Taylor Watson</a> posted comment to <strong>Awesome article</strong></div>
+                                <p>Praesent <strong>tellus elit</strong>, efficitur sit amet ex id, euismod auctor risus. <strong>Nullam a arcu tincidunt</strong>, ultrices nulla vitae, porta est.</p>
+                                
+                                <p>                                                
+                                    <a href="#" class="text-muted text-sm margin-right-10"><span class="fa fa-share-alt"></span> Share</a>
+                                    <a href="#" class="text-muted text-sm"><span class="fa fa-bullhorn"></span> Report</a>
+                                    <span class="pull-right text-muted text-sm"><i class="fa fa-clock-o"></i> 5 min ago</span>                                                                                                                
+                                </p>
+                                
+                                <div class="comments">
+                                    <div class="total">3 Comments for this post</div>
+                                    <div class="comment">
+                                        
+                                        <div class="user user--bordered user--lg">
+                                            <img src="http://via.placeholder.com/128x128">
+                                            <div class="user__name">
+                                                <a href="#" class="text-bold">Tracey Newman</a><br>
+                                                <span>Hey! This is really awesome thing. Wha do you think about it?</span>
+                                            </div>
+                                        </div>                                                    
+                                        
+                                    </div>
+                                    <div class="comment">
+                                        
+                                        <div class="user user--bordered user--lg">
+                                            <img src="http://via.placeholder.com/128x128">
+                                            <div class="user__name">
+                                                <a href="#" class="text-bold">Jonathan Foster</a><br>
+                                                <span>Well, that's a great news. Do you have anything like that?</span>
+                                            </div>
+                                        </div>
+                                                                                            
+                                    </div>
+                                    <div class="comment">
+                                        
+                                        <div class="user user--bordered user--lg">
+                                            <img src="http://via.placeholder.com/128x128">
+                                            <div class="user__name">
+                                                <a href="#" class="text-bold">Dolores Wallace</a><br>
+                                                <span>Wow, nice! Like like like!</span>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                    <div class="form">
+                                        <div class="input-group margin-bottom-0">
+                                            <input type="text" class="form-control" placeholder="Your comment..." name="comment" value="My messages goes here...">
+                                            <span class="input-group-append">
+                                                <button class="btn btn-light">Post a comment</button>
+                                            </span>
+                                        </div>                                                    
+                                    </div>
+                                </div>
+                            </div>                                                
+                        </div>
+
+                        <div class="timeline__item">
+                            <div class="user"><img src="http://via.placeholder.com/128x128" alt="Jessie Franklin"></div>
+                            <div class="content">
+                                <div class="title"><a href="#" class="text-info">Jessie Franklin</a> commented <strong>New York City</strong> photo</div>                                                    
+                                <p>Mauris tempor semper viverra. <strong>Aliquam laoreet malesuada</strong> nisl, fringilla accumsan mi condimentum a. <a href="#" class="text-info">Nullam lacinia egestas</a>. Proin iaculis malesuada mi, quis <a href="#" class="text-info">@Iaculis</a> magna tristique sed.</p>
+                                <p>
+                                    <a href="#" class="text-muted text-sm margin-right-10"><span class="fa fa-share-alt"></span> Share</a>
+                                    <a href="#" class="text-muted text-sm"><span class="fa fa-bullhorn"></span> Report</a> 
+
+                                    <span class="pull-right text-muted text-sm"><i class="fa fa-clock-o"></i> 12 min ago</span>
+                                </p> 
+
+                                <div class="comments">
+                                    <div class="total">1 Comments for this post</div>                                                        
+                                    <div class="comment">
+                                        
+                                        <div class="user user--bordered user--lg">
+                                            <img src="http://via.placeholder.com/128x128">
+                                            <div class="user__name">
+                                                <a href="#" class="text-bold">Maurice Collins</a><br>
+                                                <span>I saw this place tomorrow. Really beautiful place!</span>
+                                            </div>
+                                        </div>
+                                                                                            
+                                    </div>                                                        
+                                    <div class="form">
+                                        <div class="input-group margin-bottom-0">
+                                            <input type="text" class="form-control" placeholder="Your comment..." name="comment">
+                                            <span class="input-group-append">
+                                                <button class="btn btn-light">Post a comment</button>
+                                            </span>
+                                        </div>                                                    
+                                    </div>
+                                </div>
+                            </div>                                                
+                        </div>
+                        
+                        <div class="timeline__item">
+                            <div class="user"><img src="http://via.placeholder.com/128x128" alt=""></div>
+                            <div class="content">
+                                <div class="title"><a href="#" class="text-info">Wendy Brown</a> commented <strong>Perfection</strong> magazine article</div>                                                    
+                                <p>Curabitur vestibulum mi sit amet commodo rhoncus. <strong>Integer ullamcorper</strong>, odio id bibendum pharetra, magna arcu viverra nisi.</p>
+                                <p>
+                                    <a href="#" class="text-muted text-sm margin-right-10"><span class="fa fa-share-alt"></span> Share</a>
+                                    <a href="#" class="text-muted text-sm"><span class="fa fa-bullhorn"></span> Report</a> 
+
+                                    <span class="pull-right text-muted text-sm"><i class="fa fa-clock-o"></i> 35 min ago</span>
+                                </p> 
+
+                                <div class="comments">
+                                    <div class="total">1 Comments for this post</div>                                                        
+                                    <div class="comment">
+                                        
+                                        <p>There are no comments. Use form below to write a comment.</p>
+                                                                                            
+                                    </div>                                                        
+                                    <div class="form">
+                                        <div class="input-group margin-bottom-0">
+                                            <input type="text" class="form-control" placeholder="Your comment..." name="comment">
+                                            <span class="input-group-append">
+                                                <button class="btn btn-light">Post a comment</button>
+                                            </span>
+                                        </div>                                                    
+                                    </div>
+                                </div>
+                            </div>                                                
+                        </div>
+                        
+                        <div class="timeline__more">
+                            <a href="#">...</a>
+                        </div>
+
+                    </div>
+                                                    
+                </div>
+            </div>
+          </div>
+        </div>                    
     </c-layout>
 </template>
 
 <script>
 export default {
+  props: ['id'],
   components: {
     'c-layout': () => import('@/ui/layouts/default')
   },
-  props: [
-    'id'
-  ],
   data: () => ({
-    wallets: []
-  })
+  }),
+  created() {
+    $(".rating_readonly").raty({readOnly: true});
+  }
 }
 </script>
 
