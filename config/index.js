aa5f0889a94b2eb3082a1a81c38593b838c4c965
Eric Muyser <eric@muyser.com>
2018-07-12 16:24:01 -0700

    Rebuild
    Former-commit-id: b69cd79a2ed6d33c695233a25db53952c0964f3c

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/docs/',
+    assetsPublicPath: '/',
     templateFile: 'index.html',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
