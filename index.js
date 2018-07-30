da75c6f4a87eef71d641531e68f1cb3616372a8d
Eric Muyser <eric@muyser.com>
2018-07-29 23:26:02 -0700

    Update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import Marketplace from './smart-contracts/ethereum/models/Marketplace'
+import Marketplace from './communication-api/ethereum/models/Marketplace'
 
 export default {
     Ethereum: {
+        Contracts: {
+            Marketplace: require('./smart-contracts/ethereum/build/Marketplace.json')
+        },
         Models: {
             Marketplace: Marketplace
         }
