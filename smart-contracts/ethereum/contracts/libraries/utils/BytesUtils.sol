9e2e63d09bb20795d07f9302a8d30252a1d69f8f
Jordan Brooks <jordbrooks14@gmail.com>
2018-08-02 16:14:47 -0700

    Make lib util state variables constant.

--- smart-contracts/ethereum/contracts/libraries/utils/BytesUtils.sol
+++ smart-contracts/ethereum/contracts/libraries/utils/BytesUtils.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 library BytesUtils {
 
-    uint private BYTES_HEADER_SIZE = 32; // Can't actually use constants within in-line assembly but figured it'd be good to define anyways
+    uint private constant BYTES_HEADER_SIZE = 32; // Can't actually use constants within in-line assembly but figured it'd be good to define anyways
 
     function toBytes32(bytes _bytes) external pure returns (bytes32 retBytes32) {
         if (_bytes.length == 0) {
