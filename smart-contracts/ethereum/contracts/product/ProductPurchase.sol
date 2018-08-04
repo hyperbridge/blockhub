2730c7de138e2d2f951d4ccd05447a48375584b8
Jordan Brooks <jordbrooks14@gmail.com>
2018-08-03 19:58:09 -0700

    Purchase function in ProductPurchase creates and stores new PurchaseOrder and sends funds to developer.

--- smart-contracts/ethereum/contracts/product/ProductPurchase.sol
+++ smart-contracts/ethereum/contracts/product/ProductPurchase.sol
@@ -1,28 +1,37 @@
 pragma solidity ^0.4.24;
 
 import "./ProductBase.sol";
+import "../libraries/utils/StringUtils.sol";
 
 contract ProductPurchase is ProductBase {
 
-    event productPurchased(uint productId, uint value, address purchaser, address developer);
+    using StringUtils for string;
+
+    event productPurchased(uint productId, string pricePlanCode, uint value, address purchaser, address developer);
 
     constructor(address _marketplaceStorage) public {
         marketplaceStorage = MarketplaceStorage(_marketplaceStorage);
     }
 
-    function purchase(uint _productId, uint _pricePlanIndex) external payable {
-        uint numPricePlans = marketplaceStorage.getProductPricePlansLength(_productId);
-        require(_pricePlanIndex < numPricePlans, "Price plan does not exist.");
+    function purchase(uint _productId, string _pricePlanCode) external payable {
+        string memory registeredPricePlanCode = marketplaceStorage.getProductPricePlanCode(_productId, _pricePlanCode);
+        require(registeredPricePlanCode.equals(_pricePlanCode), "Price plan does not exist.");
 
-        uint productPrice = marketplaceStorage.getProductPricePlanPrice(_productId, _pricePlanIndex);
+        uint productPrice = marketplaceStorage.getProductPricePlanPrice(_productId, _pricePlanCode);
         require(msg.value == productPrice, "Transaction value incorrect.");
 
+        // Mark that msg.sender has purchased this product
         uint senderPurchases = marketplaceStorage.getProductHasPurchased(_productId, msg.sender);
         marketplaceStorage.setProductHasPurchased(_productId, msg.sender, senderPurchases + 1);
 
         address developerAddress = marketplaceStorage.getProductDeveloper(_productId);
+
+        // Register the purchase order
+        marketplaceStorage.pushProductPurchaseOrder(_productId, msg.sender, _pricePlanCode, msg.value, developerAddress);
+
+        // Send the funds
         developerAddress.transfer(msg.value);
 
-        emit productPurchased(_productId, msg.value, msg.sender, developerAddress);
+        emit productPurchased(_productId, _pricePlanCode, msg.value, msg.sender, developerAddress);
     }
 }
