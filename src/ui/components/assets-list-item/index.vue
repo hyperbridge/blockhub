086aa74423b4df64c78970c0010ef8d13172cd70
soxepo <bogdanvin@gmail.com>
2018-08-05 16:09:39 +0300

    Reviews Block
    Former-commit-id: ec777005147e1c713183114e82725aecd75d3ddc

---
+++ src/ui/components/assets-list-item/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="assets-list__item">
+        <div class="thumb">
+            <img src="https://d1u5p3l4wpay3k.cloudfront.net/skyrim_de_gamepedia/thumb/0/04/SteelPlateArmorofIllusion.png/200px-SteelPlateArmorofIllusion.png" />
+        </div>
+        <div class="info">
+            <h5>Magic Plate Armor</h5>
+            <span class="float-left price">
+                                    <i class="fas fa-dollar-sign"></i>
+                                    1.99
+                                </span>
+            <span class="float-right box">
+                                    <i class="fas fa-box"></i>
+                                    350. 000
+                                </span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .assets-list__item{
+        border-radius: 5px;
+        border: 1px solid rgba(255, 255, 255, 0.07);
+        background: #1C2032;
+        padding: 10px;
+        width: 100%;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, .16);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .thumb{
+            display: inline-block;
+            float: left;
+            width: 25%;
+            img{
+                width: 100%;
+                height: auto;
+            }
+        }
+        .info{
+            display: inline-block;
+            width: 70%;
+            float: right;
+            span{
+                i{
+                    margin-right: 5px;
+                }
+            }
+        }
+    }
+</style>
--- src/ui/screens/assets/index.vue
+++ src/ui/screens/assets/index.vue
@@ -1,9 +1,37 @@
 <template>
     <c-layout>
         <div class="content" id="content">
-            <div class="container-fluid">  
+            <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
+                        <div class="card">
+                            <c-heading-bar name="Get Started" :showArrows="false" :showBackground="false">
+                            </c-heading-bar>
+                            <div class="d-flex justify-content-start flex-wrap">
+                                <div class="assets-list__item-container">
+                                    <c-assets-list-item></c-assets-list-item>
+                                </div>
+                                <div class="assets-list__item-container">
+                                    <c-assets-list-item></c-assets-list-item>
+                                </div>
+                                <div class="assets-list__item-container">
+                                    <c-assets-list-item></c-assets-list-item>
+                                </div>
+                                <div class="assets-list__item-container">
+                                    <c-assets-list-item></c-assets-list-item>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <c-heading-bar name="Yours" :showArrows="false" :showBackground="false">
+                                <div class="filter_blk">
+
+                                </div>
+                            </c-heading-bar>
+                            <c-assets-grid></c-assets-grid>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -12,13 +40,30 @@
 </template>
 
 <script>
-export default {
-  components: {
-    'c-layout': () => import('@/ui/layouts/default')
-  }
-}
+    export default {
+        components: {
+            'c-layout': () => import('@/ui/layouts/default'),
+            'c-assets-grid': () => import('@/ui/components/assets-grid'),
+            'c-assets-list-item': () => import('@/ui/components/assets-list-item'),
+            'c-heading-bar': () => import('@/ui/components/heading-bar'),
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-
+    .card{
+        background: rgba(0, 0, 0, .13);
+        border-radius: 5px;
+        padding: 15px;
+        margin: 15px 0;
+        position: relative;
+        overflow: hidden;
+    }
+    .assets-list__item-container{
+        width: calc( 100%/3 );
+        padding: 5px;
+        @media only screen and (min-width: 1400px){
+            width: 25%;
+        }
+    }
 </style>
