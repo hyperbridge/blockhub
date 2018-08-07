2c7f64e72fc7798b82e17e1a150396e9ceff1324
soxepo <bogdanvin@gmail.com>
2018-08-08 01:12:40 +0300

    Wallet interactions
    Former-commit-id: 4c4cf6a042f78ff948757d78b7ec00c3a990eae2

--- src/ui/screens/account-wallets/index.vue
+++ src/ui/screens/account-wallets/index.vue
@@ -26,8 +26,8 @@
                                         <a class="dropdown-item" href="#">Something else here</a>
                                     </div>
                                 </div>
-                                <a href="#3" class="btn btn-clear">clear <i class="fas fa-times"></i></a>
-                                <a href="#3" class="btn btn-link">more filters</a>
+                                <a href="#3" class="btn btn-sm btn-danger text-uppercase font-weight-bold">clear <i class="fas fa-times"></i></a>
+                                <a href="#3" class="btn btn-sm btn-info text-uppercase font-weight-bold">more filters</a>
                             </div>
                             <div class="filter-line__sorting">
                                 Sort by:
@@ -54,7 +54,7 @@
                             <div class="wallet-item" v-for="(wallet, index) in wallets" :key="index">
                                 <a class="wallet-item__info" :href='`/#/wallet/${wallet.id}`'>
                                     <div class="wallet-item__head">
-                                        <div class="wallet-item__name">
+                                        <div class="wallet-item__name" :class="{ preferred: wallet.preferred_switcher }">
                                             <i class="fab fa-bitcoin"></i>
                                             {{ wallet.name }}
                                         </div>
@@ -95,8 +95,119 @@
                                         </label>
                                     </div>
                                     <div class="wallet-item__action">
-                                        <a class="btn btn-light btn--icon btn--icon-right">
-                                            SEND
+                                        <a class="btn btn-sm btn-info">
+                                            Send
+                                            <i class="fas fa-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--Add New Wallet Block Empty div-->
+                            <div class="wallet-item empty-item" @click="addNewWallet" v-if="!set_new_wallets">
+                                <div class="empty-overlay">
+                                    <i class="fas fa-plus"></i>
+                                    New Wallet
+                                </div>
+                            </div>
+
+                            <!--Add New Wallet Block Empty Model-->
+                            <div class="wallet-item" v-if="set_new_wallets">
+
+                                <!--Search block(currency list) off-->
+                                <div class="wallet-item__info" v-if="!search_blk">
+                                    <div class="wallet-item__head" v-if="set_new_wallets_step_1">
+                                        <div class="wallet-item__name">
+                                            <a href="#3" class="btn btn-sm wallet-item__toggle-search" @click="showCurrencyList">
+                                                <i class="fas fa-angle-down"></i>
+                                            </a>
+                                            {{ new_wallets.name }}
+                                        </div>
+                                        <div class="wallet-item__count">
+                                            {{ new_wallets.short_name }}
+                                            {{ new_wallets.count }}
+                                        </div>
+                                    </div>
+                                    <div class="wallet-item__head" v-if="set_new_wallets_step_2">
+                                        <div class="wallet-item__name" :class="{ preferred: new_wallets.preferred_switcher }">
+                                            <i class="fab fa-bitcoin"></i>
+                                            {{ new_wallets.name }}
+                                        </div>
+                                        <div class="wallet-item__count">
+                                            {{ new_wallets.short_name }} {{ new_wallets.count }}
+                                        </div>
+                                    </div>
+                                    <div class="wallet-item__body">
+                                        <div class="wallet-item__graph">
+                                        </div>
+                                        <div class="wallet-item__history">
+                                            <ul>
+                                                <li v-for="(item, index) in new_wallets.history" :class="item.direction" :key="index">
+                                                    <span class="time">
+                                                        {{ item.time }}
+                                                    </span>
+                                                    <span class="percent">
+                                                        {{ item.percent }}%
+                                                    </span>
+                                                    <span class="icon">
+                                                        <i class="fas fa-arrow-up" v-if="item.direction === 'up'"></i>
+                                                        <i class="fas fa-arrow-down" v-else></i>
+                                                    </span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="wallet-item__wallet_address">
+                                            <span id="new_wallet_number">{{ new_wallets.wallet_number }}</span>
+                                            <i class="fas fa-redo" style="color: #c25a5c" v-if="set_new_wallets_step_1" @click="generateNewWalletNumber"></i>
+                                            <i class="fas fa-copy" v-else  @click="copyWalletNumber(new_wallets.wallet_number)"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!--Search block(currency list) on-->
+                                <div class="wallet-item__search-blk" v-if="search_blk">
+                                    <div class="input-group input-group-sm">
+                                        <input type="text" class="form-control" placeholder="Search" aria-label="Search">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-search"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <ul>
+                                        <li v-for="(currency, index) in currency_choices" :key="index" @click="choseCurrency(currency.name, currency.short_name)">
+                                            <div class="float-left">
+                                                <i class="fab fa-btc"></i>
+                                                {{ currency.short_name }}
+                                            </div>
+                                            <div class="float-right">
+                                                {{ currency.name }}
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="wallet-item__footer">
+                                    <div class="wallet-item__preferred_switcher">
+                                        <label class="switch switch-sm">
+                                            <input type="checkbox" name="preeferred" checked="" v-model="new_wallets.preferred_switcher" value="0">
+                                            <span>Preferred</span>
+                                        </label>
+                                    </div>
+                                    <div class="wallet-item__action" v-if="set_new_wallets_step_1">
+                                        <a class="btn btn-sm btn-success" @click="nextStep">
+                                            Done
+                                            <i class="fas fa-check"></i>
+                                        </a>
+                                    </div>
+                                    <div class="wallet-item__action" v-else>
+                                        <a class="btn btn-sm btn-info">
+                                            <i class="fas fa-list ml-0"></i>
+                                        </a>
+                                        <a class="btn btn-sm btn-info">
+                                            <i class="fas fa-cog ml-0"></i>
+                                        </a>
+                                        <a class="btn btn-sm btn-info">
+                                            Send
                                             <i class="fas fa-arrow-right"></i>
                                         </a>
                                     </div>
@@ -114,6 +225,66 @@
 <script>
     export default {
         name: 'wallets',
+        data: function(){
+          return{
+              new_wallets:{
+                  "id": "",
+                  "name": "--",
+                  "icon": "",
+                  "short_name": "--",
+                  "count": "--",
+                  "history": [
+                      {
+                          "time": "00:00",
+                          "percent": "0.00",
+                          "direction": "up"
+                      },
+                      {
+                          "time": "00:00",
+                          "percent": "0.00",
+                          "direction": "up"
+                      },
+                      {
+                          "time": "00:00",
+                          "percent": "0.00",
+                          "direction": "up"
+                      }
+                  ],
+                  "wallet_number": "xxxxxxxxxxxxxxxxxxxx",
+                  "preferred_switcher": true
+              },
+              set_new_wallets: false,
+              set_new_wallets_step_1: false,
+              set_new_wallets_step_2: false,
+              search_blk: false,
+              currency_choices:[
+                  {
+                      name: 'Bitcoin',
+                      short_name: 'BTC'
+                  },
+                  {
+                      name: 'Ethereum',
+                      short_name: 'ETH'
+                  },
+                  {
+                      name: 'Ripple',
+                      short_name: 'XRP'
+                  },
+                  {
+                      name: 'Cardano',
+                      short_name: 'CDA'
+                  },
+                  {
+                      name: 'Thron',
+                      short_name: 'TRX'
+                  },
+                  {
+                      name: 'Bitcoin Cash',
+                      short_name: 'BCH'
+                  }
+              ]
+          }
+        },
         components: {
             'c-layout': () => import('@/ui/layouts/default')
         },
@@ -125,6 +296,30 @@
         methods: {
             copyWalletNumber: function (number) {
                 alert('You have copy wallet number - ' + number )
+            },
+            addNewWallet: function () {
+                this.set_new_wallets = true;
+                this.set_new_wallets_step_1 = true;
+            },
+            nextStep: function () {
+                this.set_new_wallets_step_1 = false;
+                this.set_new_wallets_step_2 = true;
+            },
+            generateNewWalletNumber: function () {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                for (var i = 0; i < 25; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                this.new_wallets['wallet_number'] = text;
+            },
+            showCurrencyList: function () {
+                this.search_blk = true;
+            },
+            choseCurrency: function (name, short_name) {
+                this.search_blk = false;
+                this.new_wallets['name'] = name;
+                this.new_wallets['short_name'] = short_name;
+                this.generateNewWalletNumber();
             }
         }
     }
@@ -216,24 +411,67 @@
 
     .wallets-list {
         display: flex;
-        justify-content: space-between;
         flex-wrap: wrap;
         width: 100%;
     }
 
     .wallet-item {
-        width: 32%;
+        width: 31%;
         display: inline-block;
-        margin-bottom: 2%;
+        margin: 0 1% 45px;
         background: rgba(0, 0, 0, .23);
         border: 1px solid rgba(70, 70, 70, 0.5);
         padding: 10px;
         border-radius: 5px;
         color: #fff;
+        position: relative;
+        min-height: 162px;
+        &.empty-item{
+            position: relative;
+            overflow: hidden;
+            height: 162px!important;
+            .empty-overlay{
+                content: "\f067";
+                position: absolute;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 40px;
+                font-size: 16px;
+                font-weight: bold;
+                color: #fff;
+                line-height: 40px;
+                top: calc(50% - 20px);
+                left: 0;
+                right: 0;
+                text-align: center;
+                i{
+                    color: #6d87ff;
+                    font-size: 30px;
+                    margin-right: 10px;
+                }
+            }
+        }
+        .wallet-item__footer{
+            position: absolute;
+            left: 0;
+            right: 0;
+            padding: 0 10px;
+            display: none;
+            bottom: 20px;
+            height: 22px;
+        }
+        &:not(.empty-item):hover{
+            padding-bottom: 35px;
+            margin-bottom: 10px;
+            .wallet-item__footer{
+                display: inline-block;
+            }
+        }
     }
     .wallet-item__info {
         border-radius: 5px;
-        background: rgba(255, 255, 255, .2);
+        background: #3d3f5a;
         padding: 10px;
         overflow: hidden;
         display: block;
@@ -251,19 +489,24 @@
         margin-bottom: 10px;
     }
     .wallet-item__name {
-        width: calc(100% - 95px);
+        width: calc(100% - 105px);
         text-align: left;
         i {
             font-size: 20px;
             margin-right: 5px;
             vertical-align: middle;
         }
+        &.preferred{
+            i{
+                color: #7cc27d;
+            }
+        }
     }
     .wallet-item__count {
         text-align: right;
         font-size: 16px;
         font-weight: normal;
-        width: 90px;
+        width: 100px;
     }
     .wallet-item__history {
         display: inline-block;
@@ -337,12 +580,75 @@
             padding: 0px 5px;
             color: #fff;
             font-weight: bold;
-            background: #6d87ff;
+            text-transform: uppercase;
             i{
-                margin-left: 5px;
+                margin-left: 3px;
             }
-            &:hover{
-                background: #5a71d4;
+        }
+    }
+    .wallet-item__toggle-search{
+        padding: 0px 5px;
+        color: #fff;
+        font-weight: bold;
+        text-transform: uppercase;
+        background: rgba(0, 0, 0, .2);
+        border: 1px solid rgba(0, 0, 0, .5);
+        margin-right: 5px;
+        i{
+            margin: 0;
+            font-size: 14px;
+        }
+    }
+    .wallet-item__search-blk{
+        border-radius: 5px;
+        background: #3d3f5a;
+        padding: 10px;
+        overflow: hidden;
+        display: block;
+        text-decoration: none;
+        color: #fff;
+        width: 100%;
+        height: 140px;
+        .input-group {
+            background: rgba(37, 38, 59, .1);
+            border: 1px solid rgba(37, 38, 59, .8);
+            border-radius: 5px;
+            overflow: hidden;
+            .input-group-append {
+                margin: 0;
+                .input-group-text {
+                    border: none;
+                }
+            }
+            input {
+                border: none;
+                &:active,
+                &:focus {
+                    border: unset;
+                    box-shadow: none;
+                }
+            }
+        }
+        ul{
+            padding: 0;
+            margin: 0;
+            height: calc( 100% - 40px );
+            overflow-y: auto;
+            li{
+                list-style: none;
+                display: block;
+                padding: 2px 4px;
+                overflow: hidden;
+                div{
+                    display: inline-block;
+                    i{
+                        width: 15px;
+                    }
+                }
+                &:hover{
+                    background: rgba(0, 0, 0, .1);
+                    cursor: pointer;
+                }
             }
         }
     }
