42ac5a611373840ed65831f832c5edd678f21d37
soxepo <bogdanvin@gmail.com>
2018-08-16 00:36:27 +0300

    Home page
    Former-commit-id: a10b4877ea6701a5266cb011d789933b8957ad5f

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
 
-import { storiesOf } from '@storybook/vue'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
+import {storiesOf} from '@storybook/vue'
+import {action} from '@storybook/addon-actions'
+import {linkTo} from '@storybook/addon-links'
 
 import '../css/styles.scss'
 
@@ -12,12 +12,13 @@ import MyButton from './MyButton.vue'
 import Welcome from './Welcome.vue'
 import SaleBox from '../ui/components/sale-box/box.vue'
 import PlanList from '../ui/components/game-plans/list.vue'
+import AssetsImporter from '../ui/components/asset-importer/contact-import.vue'
 
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
-  components: { Welcome },
-  template: '<welcome :showApp="action" />',
-  methods: { action: linkTo('Button') },
+    components: {Welcome},
+    template: '<welcome :showApp="action" />',
+    methods: {action: linkTo('Button')},
 }))
 
 // storiesOf('Button', module)
@@ -28,49 +29,59 @@ storiesOf('Welcome', module).add('to Storybook', () => ({
 //   }))
 
 storiesOf('Sale Box', module)
-  .add('default', () => ({
-    components: {
-      'c-sale-box': SaleBox
-    },
-    data() {
-      return {
-        sale_box: {
-          title: 'test'
-        }
-      }
-    },
-    template: '<div class="row"><div class="col-6"><c-sale-box :sale_box="sale_box"></c-sale-box></div></div>'
-  }))
+    .add('default', () => ({
+        components: {
+            'c-sale-box': SaleBox
+        },
+        data() {
+            return {
+                sale_box: {
+                    title: 'test'
+                }
+            }
+        },
+        template: '<div class="row"><div class="col-6"><c-sale-box :sale_box="sale_box"></c-sale-box></div></div>'
+    }))
 
 
 storiesOf('Plan List', module)
-  .add('default', () => ({
-    components: {
-      'c-plan-list': PlanList
-    },
-    data() {
-      return {
-        "plans": [
-          {
-            "title": "Play Tibia Now",
-            "link": "#3"
-          },
-          {
-            "title": "1 Month Premium benefits",
-            "price": "34",
-            "link": "#4"
-          },
-          {
-            "title": "2 Month Premium benefits",
-            "price": "57",
-            "link": "#4"
-          }
-        ],
-      }
-    },
-    template: '<div class="row"><div class="col-6"><c-plan-list :items="plans" /></div></div>'
-  }))
+    .add('default', () => ({
+        components: {
+            'c-plan-list': PlanList
+        },
+        data() {
+            return {
+                "plans": [
+                    {
+                        "title": "Play Tibia Now",
+                        "link": "#3"
+                    },
+                    {
+                        "title": "1 Month Premium benefits",
+                        "price": "34",
+                        "link": "#4"
+                    },
+                    {
+                        "title": "2 Month Premium benefits",
+                        "price": "57",
+                        "link": "#4"
+                    }
+                ],
+            }
+        },
+        template: '<div class="row"><div class="col-6"><c-plan-list :items="plans" /></div></div>'
+    }))
 
-
+storiesOf('Assets Importer', module)
+    .add('default', () => ({
+        components: {
+            'assets-importer': AssetsImporter
+        },
+        data() {
+            return {
+            }
+        },
+        template: '<div class="row"><div class="col-8"><assets-importer /></div></div>'
+    }))
 
 /* eslint-enable react/react-in-jsx-scope */
