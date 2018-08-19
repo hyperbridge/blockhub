f5674487006581af00ee654cb98673cf3e4ee076
soxepo <bogdanvin@gmail.com>
2018-08-20 00:30:19 +0300

    Sidebar menu + Stages progress line
    Former-commit-id: 53cf82b57b748ad5d1b7c48c1363f405770c225b

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -16,13 +16,15 @@ import AssetsImporter from '../ui/components/asset-importer/index.vue'
 import Notification from '../ui/components/notification/index.vue'
 import Tabs from '../ui/components/tab/tabs.vue'
 import Tab from '../ui/components/tab/tab.vue'
+import StagesLine from '../ui/components/stages-line/index.vue'
+import SidebarMenu from '../ui/components/sidebar-menu/index.vue'
 
 
-storiesOf('Welcome', module).add('to Storybook', () => ({
-    components: {Welcome},
-    template: '<welcome :showApp="action" />',
-    methods: {action: linkTo('Button')},
-}))
+// storiesOf('Welcome', module).add('to Storybook', () => ({
+//     components: {Welcome},
+//     template: '<welcome :showApp="action" />',
+//     methods: {action: linkTo('Button')},
+// }))
 
 // storiesOf('Button', module)
 //   .add('with text', () => ({
@@ -142,7 +144,6 @@ storiesOf('Notifications', module)
         template: '<div class="row"><div class="col-4 pt-3"><c-notification :notifications="ntf_messages" /></div></div>'
     }))
 
-
 storiesOf('Tabs', module)
     .add('default', () => ({
         components: {
@@ -153,7 +154,8 @@ storiesOf('Tabs', module)
             return {
             }
         },
-        template: '<div class="row m-0"><div class="col-8 p-4">' +
+        template: '<div class="row m-0">' +
+            '<div class="col-8 p-4">' +
             '<c-tabs>' +
             '<c-tab name="Tab 1" :selected="true" showFooter="true">' +
             '<p>This is first tab with footer content</p>' +
@@ -168,7 +170,120 @@ storiesOf('Tabs', module)
             '<p>This is third tab</p>' +
             '</c-tab>' +
             '</c-tabs>' +
-            '</div></div>'
+            '</div>' +
+            '</div>'
+    }))
+
+storiesOf('Stages Line', module)
+    .add('default', () => ({
+        components: {
+            'c-stages-line': StagesLine
+        },
+        data() {
+            return {
+                stages: [
+                    {
+                        "status": "done",
+                        "text": "m1"
+                    },
+                    {
+                        "status": "done",
+                        "text": "m2"
+                    },
+                    {
+                        "status": "in_progress",
+                        "text": "m3"
+                    },
+                    {
+                        "status": "awaiting",
+                        "text": "m4"
+                    },
+                    {
+                        "status": "awaiting",
+                        "text": "m5"
+                    },
+                    {
+                        "status": "awaiting",
+                        "text": "m6"
+                    }
+                ],
+            }
+        },
+        template: '<div class="row m-0">' +
+            '<div class="col-8 p-4">' +
+            '<c-stages-line :stages="stages" />' +
+            '</div>' +
+            '</div>'
+    }))
+
+storiesOf('Sidebar Menu', module)
+    .add('Main title', () => ({
+        components: {
+            'c-sidebar-menu': SidebarMenu
+        },
+        data() {
+            return {
+                menu: [
+                    {
+                        url: '#3',
+                        link_text: 'Link Text',
+                        target: '_self',
+                        icon: ''
+                    },
+                    {
+                        url: '#3',
+                        link_text: 'Link Text Blank target',
+                        target: '_blank',
+                        icon: ''
+                    },
+                    {
+                        url: '#3',
+                        link_text: 'Link Text with Icon',
+                        target: '_self',
+                        icon: 'fas fa-gem'
+                    }
+                ],
+            }
+        },
+        template: '<div class="row m-0">' +
+            '<div class="col-3 p-4">' +
+            '<c-sidebar-menu title="Menu Big Title" icon="fab fa-angellist" :menu="menu" />' +
+            '</div>' +
+            '</div>'
+    }))
+    .add('Sub title', () => ({
+        components: {
+            'c-sidebar-menu': SidebarMenu
+        },
+        data() {
+            return {
+                menu: [
+                    {
+                        url: '#3',
+                        link_text: 'Link Text',
+                        target: '_self',
+                        icon: ''
+                    },
+                    {
+                        url: '#3',
+                        link_text: 'Link Text Blank target',
+                        target: '_blank',
+                        icon: ''
+                    },
+                    {
+                        url: '#3',
+                        link_text: 'Link Text with Icon',
+                        target: '_self',
+                        icon: 'fas fa-gem'
+                    }
+                ],
+            }
+        },
+        template: '<div class="row m-0">' +
+            '<div class="col-3 p-4">' +
+            '<c-sidebar-menu sub_title="This sub title" sub_icon="fas fa-info-circle" :menu="menu" />' +
+            '</div>' +
+            '</div>'
     }))
 
 /* eslint-enable react/react-in-jsx-scope */
