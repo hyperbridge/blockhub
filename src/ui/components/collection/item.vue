bc575c18bdb63a86326432a750b90b1a635d6362
soxepo <bogdanvin@gmail.com>
2018-09-03 17:46:57 +0300

    Collection item
    Former-commit-id: 8c638815dd0a9e7bb1a74262a6f36432911fb652 [formerly ca86110fd5defa7fa32dab90d5c15af83bdc76e4]
    Former-commit-id: 9e6edef2719d1d68fd8383a21e96869e4a91da61

---
+++ src/ui/components/collection/item.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="collection-item" v-bind:style="{ 'background-image': 'url(' + item.background + ')' }">
+        <div class="collection-item__items-container">
+            <div v-for="(img, index) in item.images" :key="index">
+                <img :src="img"/>
+            </div>
+        </div>
+        <div class="collection-item__info">
+            <h4>{{ item.title }}</h4>
+            <h6>by {{ item.user }}</h6>
+            <div class="count">
+
+                <i class="fas fa-box"></i>
+                {{ item.count_number }} {{ item.count_number | count_label }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+ export default {
+     name: 'c-collection-item',
+     props:['item'],
+     data(){
+         return{
+             label: ''
+         }
+     },
+     filters: {
+         count_label(label) {
+             switch(label) {
+                 case label = 1:
+                     return 'Item'
+                 default:
+                     return 'Items'
+             }
+         }
+     }
+ }
+</script>
+
+<style lang="scss" scoped>
+    .collection-item{
+        color: #fff;
+        display: flex;
+        width: 100%;
+        overflow: hidden;
+        border-radius: 8px;
+        padding: 15px;
+        justify-content: center;
+        position: relative;
+        flex-direction: column;
+        box-shadow: 1px 2px 6px rgba(0, 0 , 0, .3);
+        z-index: 10;
+        background-color: rgba(0, 0, 0, .2);
+        background-repeat: no-repeat;
+        background-size: cover;
+        -webkit-transition: all 200ms ease-in;
+        -webkit-transform: scale(1);
+        -ms-transition: all 200ms ease-in;
+        -ms-transform: scale(1);
+        -moz-transition: all 200ms ease-in;
+        -moz-transform: scale(1);
+        transition: all 200ms ease-in;
+        transform: scale(1);
+        &:hover {
+            -webkit-transition: all 200ms ease-in;
+            -webkit-transform: scale(1.05);
+            -ms-transition: all 200ms ease-in;
+            -ms-transform: scale(1.05);
+            -moz-transition: all 200ms ease-in;
+            -moz-transform: scale(1.05);
+            transition: all 200ms ease-in;
+            transform: scale(1.05);
+            z-index: 20;
+        }
+        &:before{
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: rgba(48, 49, 76, .83);
+        }
+    }
+    .collection-item__items-container{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 10px;
+        z-index: 3;
+        div{
+            width: 20%;
+            background: rgba(255, 255, 255, .15);
+            padding: 5px;
+            margin: 5px;
+            position: relative;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            &:before{
+                content: "";
+                padding-top: 100%;
+                float: left;
+            }
+            img{
+                width: auto;
+                height: auto;
+                max-height: 100%;
+                max-width: 100%;
+            }
+        }
+    }
+    .collection-item__info{
+        text-align: center;
+        z-index: 3;
+        h4{
+            font-size: 15px;
+            font-weight: bold;
+            color: #fff;
+            padding: 0;
+            margin: 0;
+        }
+        h6{
+            color: #fff;
+            font-weight: normal;
+            font-size: 12px;
+        }
+        .count{
+            font-size: 15px;
+            i{
+                margin-right: 5px;
+            }
+        }
+    }
+</style>
