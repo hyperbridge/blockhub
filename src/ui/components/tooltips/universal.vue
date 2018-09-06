61ef08025279def305b2dee878a2842f3968aff2
alk831 <adam54111@gmail.com>
2018-09-06 14:14:39 +0200

    finish tooltip universal component
    Former-commit-id: 56515bda7492d96509709ce84098d14d4c4036c6 [formerly fa87e4d23f25d4cb8f23e2e7175a73af64caa3d5]
    Former-commit-id: f582f90196b46676cc1929245b1f42c23a54c53a

--- src/ui/components/tooltips/universal.vue
+++ src/ui/components/tooltips/universal.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        {{ show_tooltip }}
+    <div class="tooltip-universal__wrapper">
         <div
             class="tooltip-universal__content"
             @mouseover="show_tooltip = true"
             @mouseout="show_tooltip = false"
         >
             <slot/>
+            <i v-if="!icon_hide" class="fas fa-question-circle"></i>
         </div>
-        <div class="tooltip-universal" v-show="show_tooltip" >
-            <slot name="tooltip"/>
+        <div class="tooltip-universal" :class="position" v-if="show_tooltip">
+            <slot name="tooltip">
+                <div class="tooltip-universal__default" :class="[position, theme, default_class]">
+                    <h4 v-if="title">{{ title }}</h4>
+                    {{ text }}
+                </div>
+            </slot>
         </div>
     </div>
 </template>
@@ -20,8 +25,16 @@ export default {
     props: {
         position: {
             type: String,
-            default: 'left'
-        }
+            default: 'top'
+        },
+        text: String,
+        title: String,
+        theme: {
+            type: String,
+            default: 'light'
+        },
+        icon_hide: Boolean,
+        default_class: String
     },
     data() {
         return {
@@ -32,31 +45,98 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .tooltip-universal {
+    .tooltip-universal__content {
         position: relative;
+        .fa-question-circle {
+            position: absolute;
+            font-size: 10px;
+            top: 0;
+            right: -12px;
+        }
     }
+
+    .tooltip-universal__wrapper {
+        position: relative;
+        display: inline-flex;
+    }
+
+    $distance: 10px;
+    $triangle: 15px;
+    $triangleHalf: $triangle / 2;
+    $triangleDist: $triangleHalf + 2;
 
     .tooltip-universal {
         position: absolute;
-        width: 360px;
-        border-radius: 5px;
-        color: #fff;
-        background: #1C2032;
-        text-align: left;
         z-index: 999;
-        top: 100%;
+        animation: slide-fade .15s ease;
         &.top {
+            bottom: calc(100% + #{$distance});
+        }
+        &.right {
+            left: calc(100% + #{$distance});
+        }
+        &.bottom {
+            top: calc(100% + #{$distance});
+        }
+        &.left {
+            right: calc(100% + #{$distance});
+        }
+    }
+
+    .tooltip-universal__default {
+        padding: 10px;
+        border-radius: 8px;
+        box-shadow: 0 0 5px rgba(28, 32, 50, .6);
+        min-width: 140px;
+        h4 {
+            margin-top: 0;
+            margin-bottom: 5px;
+        }
+        &.dark {
+            background-color: #1c2032;
+            &:before {
+                background-color: #1C2032;
+            }
+        }
+        &.light {
+            background-color: #5D75F7;
+            &:before {
+                background-color: #5D75F7;
+            }
+        }
+        &.top:before {
+            bottom: $triangleHalf * -1;
+            left: $triangleDist;
+        }
+        &.right:before {
+            left: $triangleHalf * -1;
+            top: $triangleDist;
+        }
+        &.bottom:before {
+            top: $triangleHalf * -1;
+            left: $triangleDist;
+        }
+        &.left:before {
+            right: $triangleHalf * -1;
+            top: $triangleDist;
         }
         &:before {
-            width: 30px;
-            height: 30px;
+            width: $triangle;
+            height: $triangle;
             position: absolute;
-            left: -15px;
-            top: calc(50% - 15px);
             content: "";
-            background: #3D3E5D;
             transform: rotate(45deg);
-            box-shadow: -7px 7px 14px rgba(0, 0, 0, .2);
+        }
+    }
+
+    @keyframes slide-fade {
+        from {
+            opacity: 0;
+            transform: translateY(-5px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0);
         }
     }
 </style>
