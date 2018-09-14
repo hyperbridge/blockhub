1fac6c318ac3f3706e64b9c9360419e37d75ed98
soxepo <bogdanvin@gmail.com>
2018-09-14 18:10:13 +0300

    component for list items
    Former-commit-id: 5eb5c4995ff95e455e0ae5bad7117e5587a7d69e [formerly 02e6eb963a50ba80aad6115be37c68e62b9839a7]
    Former-commit-id: be66e0729595bdede3b0db411434ba4d99b44bc7

---
+++ src/ui/components/help/simple-item.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="simple-list-item">
+        <a :href="link">
+            <slot />
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'simple-list-item',
+        props:['link']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .simple-list-item{
+        padding: 5px 0;
+        width: 100%;
+        a{
+            color: #fff;
+        }
+    }
+</style>
