6c144c67ff1cc3b8b4f667963fc64e1d9deab486
alk831 <adam54111@gmail.com>
2018-09-26 11:32:39 +0200

    fix debouncer, add array handler mixin

--- src/mixins.js
+++ src/mixins.js
@@ -1,13 +1,24 @@
 export const debouncer = {
     data() {
         return {
-            timeout: null
+            timeout: 0
         }
     },
     methods: {
-        debounce(fn, debounceTime = 250, timeout = this.timeout) {
-            clearTimeout(timeout);
-            timeout = setTimeout(fn, debounceTime);
+        debounce(fn, debounceTime = 250, timeout = 'timeout') {
+            clearTimeout(this[timeout]);
+            this[timeout] = setTimeout(fn, debounceTime);
+        }
+    }
+};
+
+export const arrayHandler = {
+    methods: {
+        handleArray(val, arrayName) {
+            const itemIndex = this[arrayName].indexOf(val);
+            itemIndex > -1
+                ? this[arrayName].splice(itemIndex, 1)
+                : this[arrayName].push(val);
         }
     }
 };
