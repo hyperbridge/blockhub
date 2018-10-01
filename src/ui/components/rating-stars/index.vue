4be80975275341289f82ceb9ce51bc80950da28f
alk831 <adam54111@gmail.com>
2018-10-01 17:36:28 +0200

    update rating stars comp

--- src/ui/components/rating-stars/index.vue
+++ src/ui/components/rating-stars/index.vue
@@ -1,33 +1,33 @@
 <template>
     <span>
-        <i
+        <c-icon
             v-for="(star, index) in stars"
             :key="index"
-            class="fas"
-            :class="`fa-${star}`"
-        ></i>
+            :name="star"
+        />
     </span>
 </template>
 
 <script>
-export default {
-    name: 'rating-stars',
-    props: {
-        number: {
-            required: true
-        }
-    },
-    computed: {
-        stars() {
-            const stars = [];
-            const parsed_number = Math.floor(this.number).toString();
-            for (let i = 0; i < parsed_number; i++) {
-                stars.push('star');
+    export default {
+        name: 'rating-stars',
+        props: {
+            number: {
+                type: Number,
+                required: true
             }
-            return Number.isInteger(this.number) ? stars : [...stars, 'star-half'];
+        },
+        computed: {
+            stars() {
+                const stars = [];
+                const parsed_number = Math.floor(this.number).toString();
+                for (let i = 0; i < parsed_number; i++) {
+                    stars.push('star');
+                }
+                return Number.isInteger(this.number) ? stars : [...stars, 'star-half'];
+            }
         }
     }
-}
 </script>
 
 
