58f02e59ea1b446a4962cdb712269681d8874256
soxepo <bogdanvin@gmail.com>
2018-10-12 19:00:28 +0300

    promo box minor changes

--- src/ui/components/promotion-box/item.vue
+++ src/ui/components/promotion-box/item.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="promotion-item" :class="{ 'bottom-border': border}">
         <div class="promotion-item__icon">
-            <i :class="['fas fa-' + icon]" :style="['color: #' + iconColor]"></i>
+            <i class="fas fa-star" v-if="basic" style="color: #FADC72"></i>
+            <i class="fas fa-shopping-cart" v-else></i>
         </div>
-        <h5 class="promotion-item__title" v-if="title">
+        <h5 class="promotion-item__title" :class="{'font-weight-bold' : basic}" v-if="title">
             {{ title }}
         </h5>
         <c-button
@@ -61,8 +62,7 @@ export default {
 <style lang="scss" scoped>
     .promotion-item {
         /*border: 2px solid #fbe17d;*/
-        border-radius: 5px;
-        padding: 5px 10px 15px 10px;
+        padding: 5px 0px 15px 0px;
         margin-bottom: 10px;
         display: flex;
         justify-content: space-between;
@@ -73,14 +73,12 @@ export default {
     }
     .promotion-item__icon{
         font-size: 18px;
-        color: #fff;
-        opacity: .4;
+        color: #C6C6D6;
         width: 30px;
         text-align: left;
     }
     .promotion-item__title {
         font-size: 21px;
-        font-weight: bold;
         padding: 0;
         margin: 0;
         color: #fff;
