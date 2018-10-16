e1521977a892a3a6f3f27a8c70368d74eab82225
alk831 <adam54111@gmail.com>
2018-10-16 17:21:41 +0200

    add step2 asset preview, from button handlers, global css helpers

--- src/ui/screens/marketplace/search/sniper/index.vue
+++ src/ui/screens/marketplace/search/sniper/index.vue
@@ -5,7 +5,7 @@
         <c-tabs tabText="Step" class="margin-bottom-100" :setActiveTab="activeStep" @click="createSniper">
 
             <c-tab :tab_id="1">
-                <p>Select asset from market that you are going to buy</p>
+                <p>Select asset that you are going to buy</p>
                 <c-content-navigation :items="assets" :setLimits="10">
                     <c-asset-grid
                         slot-scope="props"
@@ -16,16 +16,28 @@
             </c-tab>
 
             <c-tab :tab_id="2">
-                <!-- Selected asset... -->
+                <h4 class="text-align-center">Selected asset:</h4>
+                <c-asset-preview-price
+                    :asset="assets[newSniper.asset]"
+                    class="margin-auto margin-bottom-30 half-width"
+                />
                 <p>Select minimum price</p>
                 <div class="flex-center-between margin-bottom-20">
                     <c-range-slider v-model="newSniper.priceMin" class="half-width margin-right-20"/>
                     <c-input v-model="newSniper.priceMin" class="half-width"/>
                 </div>
                 <p>Select maximum price</p>
-                <div class="flex-center-between">
+                <div class="flex-center-between margin-bottom-40">
                     <c-range-slider v-model="newSniper.priceMax" class="half-width margin-right-20"/>
                     <c-input v-model="newSniper.priceMax" class="half-width"/>
+                </div>
+                <div class="flex-center-between">
+                    <c-button status="warning" @click="cancelCreation()" icon="trash-alt" size="lg">
+                        Cancel
+                    </c-button>
+                    <c-button status="success" @click="createSniper()" size="lg">
+                        Create
+                    </c-button>
                 </div>
             </c-tab>
 
@@ -43,7 +55,7 @@
             </thead>
             <tbody>
                 <tr v-for="(sniper, id) in snipers" :key="id">
-                    <template v-if="edited.id !== sniper.id">
+                    <template v-if="edited.id != sniper.id">
                         <td>{{ id }}</td>
                         <td><c-asset-preview :asset="sniper.asset" size="sm"/></td>
                         <td>{{ sniper.priceMin }} $</td>
@@ -59,12 +71,12 @@
                     </template>
                     <template v-else>
                         <td>{{ id }}</td>
-                        <td>{{ edited.asset.name }}</td>
+                        <td><c-asset-preview :asset="sniper.asset" size="sm"/></td>
                         <td><c-input v-model="edited.priceMin" class="edit-input"/></td>
                         <td><c-input v-model="edited.priceMax" class="edit-input"/></td>
                         <td><c-input v-model="edited.expDate" class="edit-input"/></td>
                         <td>
-                            <c-button status="warning" @click="edited = {}">Cancel</c-button>
+                            <c-button status="warning" @click="edited = {}" icon_hide>Cancel</c-button>
                             <c-button status="danger" @click="deleteSniper(id)">Delete</c-button>
                             <c-button status="success" @click="updateSniper(id)">Save</c-button>
                         </td>
@@ -80,6 +92,7 @@
         components: {
             'c-range-slider': (resolve) => require(['@/ui/components/range-slider/pure'], resolve),
             'c-asset-preview': (resolve) => require(['@/ui/components/asset/preview-basic'], resolve),
+            'c-asset-preview-price': (resolve) => require(['@/ui/components/asset-preview'], resolve),
             'c-asset-grid': (resolve) => require(['@/ui/components/assets-grid-inventory'], resolve),
             'c-content-navigation': (resolve) => require(['@/ui/components/content-navigation'], resolve),
             'c-tab': (resolve) => require(['@/ui/components/tab/tab-universal'], resolve),
@@ -107,7 +120,7 @@
                 if (activeStep === 1) {
 
                 } else {
-                    if (tabId <= activeStep) {
+                    if (tabId < activeStep) {
                         this.activeStep = tabId;
                     }
                 }
@@ -136,6 +149,15 @@
             },
             deleteSniper(id) {
                 this.$store.commit('assets/delete', { id, prop: 'snipers' });
+            },
+            cancelCreation() {
+                this.activeStep = 1;
+                this.newSniper = {
+                    asset: null,
+                    priceMin: 0,
+                    priceMax: 0,
+                    expDate: ''
+                };
             }
         },
         computed: {
