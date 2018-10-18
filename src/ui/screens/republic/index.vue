596082bb39b372f0bec0c538eeac36977647e0bc
Eric Muyser <eric@muyser.com>
2018-10-18 17:56:31 +0900

    Republic

--- src/ui/screens/republic/index.vue
+++ src/ui/screens/republic/index.vue
@@ -1,17 +1,36 @@
 <template>
-    <div>todo</div>
+    <c-layout>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-6 mb-4 mb-lg-0">
+                    <c-card class="text-center">
+                        <h4 class="h2">The Republic</h4>
+                        
+                    </c-card>
+                </div>
+            </div>
+        </div>
+    </c-layout>
 </template>
 
 <script>
-export default {
-  components: {
-  },
-  data: () => ({
-    wallets: []
-  })
-}
+    export default {
+        props: ['id'],
+        components: {
+            'c-layout': (resolve) => require(['@/ui/layouts/default'], resolve),
+            'c-block': (resolve) => require(['@/ui/components/block'], resolve),
+            'c-article-item': (resolve) => require(['@/ui/components/help/article-item'], resolve),
+            'c-topic-item': (resolve) => require(['@/ui/components/help/topic-item'], resolve),
+            'c-list-item': (resolve) => require(['@/ui/components/help/simple-item'], resolve),
+            'c-card': (resolve) => require(['@/ui/components/help/help-card.vue'], resolve),
+        },
+        methods: {
+        },
+        computed: {
+
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
