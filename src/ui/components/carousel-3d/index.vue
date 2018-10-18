46d390c1ddf0028f376d0e59cb71fe85caab4c88
alk831 <adam54111@gmail.com>
2018-10-18 12:43:59 +0200

    add carousel logic

--- src/ui/components/carousel-3d/index.vue
+++ src/ui/components/carousel-3d/index.vue
@@ -39,12 +39,52 @@
         },
         computed: {
             visibleItems() {
+                return 4;
                 const { items, focusItem } = this;
                 const visible = [];
 
-                // visible.push(items[focustItem - 1])
+                visible.push(items[focustItem - 1]);
+                visible.push(items[focusItem]);
+                visible.push(items[focusItem + 1]);
 
                 return items.slice(focusItem - 1, focusItem + 2);
+            },
+            focusItems() {
+                return 4;
+                const slicer = (array, start = 0, end = array.length) => array
+                    .filter((item, index) => {
+                        const { length } = array;
+
+                        const started = start >= 0
+                        ? start
+                        : array[length + start]
+
+                        if (start < 0) return index >= started || index <= end;
+                        else return index >= started && index <= end;
+                    });
+
+                const { focusItem } = this;
+                const focused = focusItem
+                return [focusItem, focusItem + 1, focusItem + 2];
+            },
+            activeItems() {
+                const { items, focusItem, limitTo } = this;
+                const { length } = items;
+
+                let focused = focusItem % length;
+                if (focused < 0) focused *= -1;
+
+                const itemsPerSide = (limitTo - 1) / 2;
+
+                const start = focused - itemsPerSide < 0
+                    ? assets.slice(length + focused - itemsPerSide, length)
+                    : assets.slice(focused - itemsPerSide, focused);
+
+                const end = focused + 1 >= length
+                    ? assets.slice(0, length - focused)
+                    : assets.slice(focused + 1, focused + itemsPerSide + 1)  ;
+
+                return [...start, items[focused], ...end];
             }
         }
     }
