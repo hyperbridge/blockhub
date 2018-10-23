2b5c3e59de6a0e56ebf1eaf19cdeabd5b1c744e2
Eric Muyser <eric@muyser.com>
2018-10-23 16:49:27 +0900

    Remove mcustomscrollbar and owl carousel

--- src/ui/components/layout/scripts-important.vue
+++ src/ui/components/layout/scripts-important.vue
@@ -3,8 +3,6 @@
     <script type="text/javascript" src="js/vendors/jquery/jquery.min.js"></script>
     <script type="text/javascript" src="js/vendors/jquery/jquery-migrate.min.js"></script>
     <script type="text/javascript" src="js/vendors/bootstrap/bootstrap.bundle.min.js"></script>
-
-    <script type="text/javascript" src="js/vendors/mcustomscrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
     <!-- END IMPORTANT SCRIPTS --> 
 </template>
 
