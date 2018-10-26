43ac373012aa5dc771812a724d484b955af67f98
soxepo <bogdanvin@gmail.com>
2018-10-26 20:40:44 +0300

    Terms popup + minor changes

--- src/ui/components/popups/terms.vue
+++ src/ui/components/popups/terms.vue
@@ -1,20 +1,25 @@
 <template>
     <transition name="fade" :duration="100">
-        <div class="c-basic-popup" v-if="activated" @click.self.prevent="$emit('close')">
-            <div class="c-basic-popup__item" :style="{ 'width': + width + 'px'}">
+        <div class="c-terms-popup" v-if="activated" @click.self.prevent="$emit('close')">
+            <div class="c-terms-popup__item" :style="{ 'width': + width + 'px'}">
                 <div class="position-relative">
-                    <div class="c-basic-popup__close" @click="$emit('close')">
+                    <div class="c-terms-popup__close" @click="$emit('close')">
                         <i class="fas fa-times"></i>
                     </div>
-                    <div class="c-basic-popup__content">
-                        <div class="c-basic-popup__header">
+                    <div class="c-terms-popup__content">
+                        <div class="c-terms-popup__header">
                             <slot name="header" />
                         </div>
-                        <div class="c-basic-popup__body">
+                        <div class="c-terms-popup__body" id="terms-popup__body" @scroll="checkReadState">
                             <slot name="body" />
                         </div>
-                        <div class="c-basic-popup__footer">
-                            <slot name="footer" />
+                        <div class="c-terms-popup__footer">
+                            <c-button status="danger">
+                                Cancel
+                            </c-button>
+                            <c-button status="success" :class="{'not-accept' : !isRead}">
+                                Accept
+                            </c-button>
                         </div>
                     </div>
                 </div>
@@ -25,19 +30,34 @@
 
 <script>
     export default {
-        name: 'c-basic-popup',
+        name: 'c-terms-popup',
         props:{
             activated:{
                 type: Boolean,
                 default: false
             },
             width: String
+        },
+        data(){
+            return{
+                isRead: false
+            }
+        },
+        methods:{
+            checkReadState(){
+                if(($('#terms-popup__body').scrollTop() + $('#terms-popup__body').innerHeight()) >= ($('#terms-popup__body')[0].scrollHeight)) {
+                    console.log('1')
+                    this.isRead = true
+                } else {
+                    this.isRead = false
+                }
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    .c-basic-popup {
+    .c-terms-popup {
         display: flex;
         width: 100%;
         height: 100vh;
@@ -56,7 +76,7 @@
         }
     }
 
-    .c-basic-popup__close {
+    .c-terms-popup__close {
         position: absolute;
         top: -25px;
         right: 0px;
@@ -78,7 +98,7 @@
         }
     }
 
-    .c-basic-popup__item {
+    .c-terms-popup__item {
         display: flex;
         position: relative;
         z-index: 30;
@@ -88,7 +108,7 @@
         color: #fff;
     }
 
-    .c-basic-popup__content {
+    .c-terms-popup__content {
         background: #1C2032;
         padding: 10px;
         border-radius: 5px;
@@ -96,7 +116,7 @@
         box-shadow: 0 0 10px rgba(0, 0, 0, .3);
     }
 
-    .c-basic-popup__header{
+    .c-terms-popup__header{
         padding-bottom: 10px;
         margin-bottom: 10px;
         border-bottom: 1px solid rgba(255, 255, 255, .1);
@@ -110,7 +130,7 @@
             margin: 0;
         }
     }
-    .c-basic-popup__body{
+    .c-terms-popup__body{
         display: flex;
         flex-direction: column;
         overflow-y: auto;
@@ -118,5 +138,16 @@
         color: #fff;
         max-height: 60vh;
         margin: 20px 0;
+    }
+    .c-terms-popup__footer{
+        display: flex;
+        justify-content: flex-end;
+        .c-btn{
+            margin-left: 10px;
+            &.not-accept{
+                pointer-events: none;
+                opacity: .5;
+            }
+        }
     }
 </style>
