81f206016aebef8c5d07ffcd01b77979afcd1f1a
alk831 <adam54111@gmail.com>
2018-10-30 17:48:57 +0100

    add vertical & horizontal lines + arrows

--- src/ui/components/item-navigator/item.vue
+++ src/ui/components/item-navigator/item.vue
@@ -2,6 +2,7 @@
     <div
         class="navigator-item"
         :class="{
+            'navigator-item--first-row': !isChildren,
             'navigator-item--first': index === 0,
             'navigator-item--last': index === listLength - 1
         }"
@@ -11,7 +12,10 @@
             {{ isChildren && 'child' }}
             {{ index }} {{ listLength }} {{ item.evolvesTo.length }}
         </div>
-        <div class="navigator-item__sub-navigators">
+        <div
+            class="navigator-item__sub-navigators"
+            :class="{ 'sub-navigators__line': item.evolvesTo.length }"
+        >
             <navigator-item
                 v-for="(subItem, index) in item.evolvesTo"
                 :index="index"
@@ -41,6 +45,20 @@
         display: flex;
         position: relative;
 
+        &:after {
+            position: absolute;
+            font-family: 'Font Awesome 5 Free';
+            font-weight: 900;
+            font-size: 15px;
+            content: "\F061";
+            left: 8px;
+            // top: calc(65px - 10px);
+            top: 55px;
+            // top: 0;
+            // left: -30px;
+        }
+
+
         &:before {
             content: "";
             position: absolute;
@@ -66,5 +84,34 @@
         height: 70px;
         background: red;
         margin: 30px;
+    }
+    .navigator-item__sub-navigators {
+        position: relative;
+        &.sub-navigators__line {
+            &:before {
+                position: absolute;
+                content: "";
+                width: 2px;
+                height: calc(100% - 120px);
+                top: 60px;
+                background: blue;
+            }
+            &:after {
+                position: absolute;
+                content: "";
+                height: 2px;
+                width: 30px;
+                background: green;
+                left: -30px;
+                top: 60px;
+                box-shadow: -15px 0 30px 1px rgb(49, 156, 255);
+            }
+        }
+    }
+
+    .navigator-item--first-row {
+        &:before, &:after {
+            display: none;
+        }
     }
 </style>
