e760844e697a921f9848b610611703992f225bf0
alk831 <adam54111@gmail.com>
2018-10-30 13:24:14 +0100

    upgrade game installer comp & add saved paths to account seed data

--- src/ui/components/game-installer/index.vue
+++ src/ui/components/game-installer/index.vue
@@ -6,7 +6,7 @@
                 :src="product.images.medium_tile"
             />
             <div class="game-installer__description">
-                <h3>{{ product.name }} - {{ product.publisher }}</h3>
+                <h3>{{ product.name }}</h3>
                 <p v-html="description"></p>
                 <c-icon
                     v-for="(reqs, index) in product.system_requirements"
@@ -22,27 +22,43 @@
                 Location:
             </span>
             <div class="game-installer__input-wrapper">
+                <c-icon name="folder-open"/>
                 <input
-                    class="game-installer__file-dest"
-                    type="text"
+                    class="game-installer__input"
+                    v-model="path"
                     list="saved-paths"
-                    v-model="location"
                 />
-                <c-dropdown>
-                    <c-list :items="savedLocations" @click="location = $event">
-                        <span slot-scope="props">
-                            {{ props.item }}
-                        </span>
-                    </c-list>
-                </c-dropdown>
+                <datalist id="saved-paths">
+                    <option v-for="(path, index) in savedPaths" :key="index">
+                        {{ path }}
+                    </option>
+                </datalist>
             </div>
         </div>
+
+
+
+        <div class="game-installer__space-info">
+            <p>
+                Disk space required
+                <span class="game-installer__size">512 MiB</span>
+            </p>
+            <p>
+                Disk space available
+                <span class="game-installer__size">53 GiB</span>
+            </p>
+        </div>
+
 
         <div class="game-installer__buttons-wrapper">
             <button type="button" class="game-installer__button">
                 Cancel
             </button>
-            <button type="button" class="game-installer__button--install">
+            <button
+                type="button"
+                class="game-installer__button--install"
+                :class="{ 'disabled': !path }"
+            >
                 <c-icon name="hdd"/>
                 <span>Install</span>
             </button>
@@ -60,8 +76,7 @@
         },
         data() {
             return {
-                location: '/home/leafo/.config/itch/apps',
-                savedLocations: ['ww', 'aa']
+                path: '',
             }
         },
         methods: {
@@ -69,6 +84,10 @@
                 switch(os) {
                     case 'win':
                         return 'windows';
+                    case 'mac':
+                        return 'apple';
+                    case 'linux':
+                        return 'linux';
                     default: return '';
                 }
             }
@@ -76,6 +95,9 @@
         computed: {
             description() {
                 return this.product.content.substring(0, 150) + '...';
+            },
+            savedPaths() {
+                return this.$store.state.application.account.saved_paths;
             }
         }
     }
@@ -87,64 +109,80 @@
         display: flex;
         border: 1px solid rgba(255,255,255,.08);
         margin-bottom: 20px;
+        .fab {
+            margin-right: 10px;
+        }
     }
     .game-installer__image {
         width: 160px;
-        max-height: 170px;
+        max-height: 160px;
         object-fit: cover;
         background-position: 0 0;
     }
-    .game-installer {
-
-    }
     .game-installer__description {
         padding: 20px;
-    }
-    .game-installer__file-dest {
-        width: 80%;
-        padding: 7px 7px 7px 37px;
-        background: url("../../../assets/SVG/folder-open-regular.svg") 7px center no-repeat;
-        background-size: 20px;
-        border-style: none;
     }
 
     .game-installer__input-wrapper {
         display: flex;
         align-items: center;
         background-color: #28283B;
-        padding: 4px;
+        padding: 4px 10px;
         justify-content: space-between;
         border: 1px solid rgba(255,255,255,.2);
         width: 100%;
+        .fas {
+            font-size: 16px;
+        }
+        .game-installer__input {
+            width: 100%;
+            background: #28283B;
+            padding: 5px;
+            margin-left: 10px;
+            border: none;
+        }
     }
-    .game-installer__opt-title {
-        margin-right: 30px;
-    }
+
     .game-installer__opt-wrapper {
         display: flex;
         align-items: center;
+        .game-installer__opt-title {
+            margin-right: 30px;
+        }
+    }
+
+    .game-installer__space-info {
+        margin: 35px 0;
+        .game-installer__size {
+            color: #fff;
+            margin-left: 4px;
+        }
     }
 
     .game-installer__buttons-wrapper {
         display: flex;
         justify-content: space-between;
-        margin-top: 40px;
-    }
-    .game-installer__button {
-        padding: 7px 20px;
-        border-radius: 4px;
-        border: 1px solid #8c92ff;
-        border-top-width: 2px;
-        background: linear-gradient(-115deg,#30336b, #130f40);
-        color: #fff;
-        cursor: pointer;
-        text-shadow: 2px 2px 0px rgba(1,1,1,.4);
-    }
-    .game-installer__button--install {
-        @extend .game-installer__button;
-        box-shadow: 0 0 30px 0 rgba(140, 146, 255, .45);
-        .fas {
-            margin-right: 5px;
+        .game-installer__button {
+            padding: 7px 20px;
+            border-radius: 4px;
+            border: 1px solid #8c92ff;
+            border-top-width: 2px;
+            background: linear-gradient(-115deg,#30336b, #130f40);
+            color: #fff;
+            cursor: pointer;
+            text-shadow: 2px 2px 0px rgba(1,1,1,.4);
+        }
+        .game-installer__button--install {
+            @extend .game-installer__button;
+            box-shadow: 0 0 30px 0 rgba(140, 146, 255, .45);
+            .fas {
+                margin-right: 5px;
+            }
+        }
+        .disabled {
+            pointer-events: none;
+            filter: grayscale(100%);
+            box-shadow: none;
         }
     }
 </style>
