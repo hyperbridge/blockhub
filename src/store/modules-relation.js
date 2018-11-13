b740e023a4e224483c1a36e464b9b5cb003660f5
alk831 <adam54111@gmail.com>
2018-11-13 16:49:11 +0100

    scaffold decomposer helper

--- src/store/modules-relation.js
+++ src/store/modules-relation.js
@@ -34,5 +34,32 @@ export const findRelation = (module, target, prop) =>
     || [module, prop];
 
 
+const decompose = (data, destination) => {
+    const [module, target] = destination.split('/');
+    const relation = relations[module][target];
+
+    const decomposed = data.map(item => {
+        const decompItem = { ...item };
+
+        for (let key in item) {
+            if (relation && relation[key]) {
+                decompItem[key].map(val => val.id);
+            }
+        }
+
+
+    });
+};
+
+
+const data = [
+    { id: 1, yourOffer: [{ id: 1, name: 'Armor'}, { id: 1, name: 'Armor'}] },
+    { id: 2, yourOffer: [{ id: 3, name: 'Armor'}, { id: 4, name: 'Armor'}] },
+]
+
+
+decompose(data, ['assets/transactions']);
+
+
 
 export default relations;
