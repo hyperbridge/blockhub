a706524ce4c70b7c6b8a0a4bb15f441fd307e7c3
alk831 <adam54111@gmail.com>
2018-11-16 11:29:50 +0100

    add offer/id route

---
+++ src/ui/screens/marketplace/offers/_id/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="asset">
+            <h1>{{ asset.name }}</h1>
+
+
+        </div>
+        <p v-else>Asset with id {{ id }} doesn't exist</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['id'],
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+            asset() {
+                return this.$store.getters['assets/assets'][this.id];
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
