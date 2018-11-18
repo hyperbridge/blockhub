15abcba5b9c506dbf41d0b7459d480b1037f9a80
Eric Muyser <eric@muyser.com>
2018-11-18 13:49:37 +0900

    HTML editor component

---
+++ src/ui/components/html-editor/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <textarea class="form-control" :name="name"></textarea>
+</template>
+<script>
+    export default {
+        props: {
+            model: {
+                required: true,
+            },
+
+            name: {
+                type: String,
+                required: true,
+            },
+
+            height: {
+                type: String,
+                default: '150'
+            }
+        },
+
+        mounted() {
+
+        
+            let config = {
+                height: this.height
+            };
+
+            let vm = this;
+
+            config.callbacks = {
+
+                onInit: function () {
+                    $(vm.$el).summernote("code", vm.model);
+                },
+
+                onChange: function () {
+                    vm.$emit('update:model', $(vm.$el).summernote('code'));
+                    vm.$emit('change', $(vm.$el).summernote('code'));
+                },
+
+                onBlur: function () {
+                    vm.$emit('update:model', $(vm.$el).summernote('code'));
+                    vm.$emit('change', $(vm.$el).summernote('code'));
+                }
+            };
+
+            $(this.$el).summernote(config);
+
+        },
+    }
+</script>
