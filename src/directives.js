d1e2e5b31deceb6b219ab2fea418714e782dbf48
alk831 <adam54111@gmail.com>
2018-11-19 15:53:33 +0100

    cleanup direcrtives, deal old

--- src/directives.js
+++ src/directives.js
@@ -2,51 +2,6 @@ import Vue from 'vue';
 import store from './store';
 import axios from 'axios';
 
-Vue.directive('xmodel', {
-    bind(el, binding, vnode) {
-        const { context: ctx } = vnode;
-        const { arg: event = 'input', expression, modifiers } = binding;
-
-        const [element, prop] = expression.split('.');
-        const modifierKeys = Object.keys(modifiers);
-        const debounce = modifierKeys.find(key => key.includes('debounce'));
-        let timeout = null;
-
-        // vnode.componentOptions.propsData = ctx[element][prop + 'Clone'];
-        // console.log(ctx)
-
-        el.updateValue = e => {
-            let value = (e.target && e.target.value) || e;
-            if (modifiers.number) value = Number(value);
-
-            if (debounce) {
-                const [, time] = debounce.split('-');
-                clearTimeout(timeout);
-                timeout = setTimeout(() =>
-                    ctx[element] = { [prop]: value },
-                    time * 100 || 300
-                );
-            } else {
-                ctx[element] = { [prop]: value };
-            }
-
-
-            // ctx[element + 'Clone'][prop] = value;
-
-            // el.target.value = value;
-            // vnode.componentOptions.propsData.value = value;
-            // vnode.componentInstance.value = value;
-
-            console.log(vnode)
-        };
-
-        el.addEventListener(event, el.updateValue);
-    },
-    unbind(el, { arg: event = 'input' }) {
-        el.removeEventListener(event, el.updateValue);
-    }
-});
-
 Vue.directive('focus', {
     inserted(el) {
         el.focus();
@@ -89,12 +44,8 @@ Vue.directive('click-outside', {
         const { expression, arg } = binding;
         const clickOutside = Symbol.for('clickOutside');
 
-
         el[clickOutside] = evt => {
-            console.log(arg)
-
             if (arg && arg == 'self') {
-                console.log("SELFF")
                 vnode.context[expression](evt);
             } else if (!(el == evt.target || el.contains(evt.target))) {
                 vnode.context[expression](evt);
