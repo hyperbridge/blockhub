e6893095c24b0cb7db310dcb9ae3dc408995d07e
Eric Muyser <eric@muyser.com>
2018-11-22 15:36:19 +0900

    Remove build tracking

--- services/web/build/models/project-member.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const objection_1 = require("objection");
-class ProjectMemberModel extends objection_1.Model {
-    static get tableName() {
-        return 'project_members';
-    }
-}
-exports.default = ProjectMemberModel;
