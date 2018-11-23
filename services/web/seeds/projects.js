75864c579c431d28389aa6b9297f447924f5a657
Eric Muyser <eric@muyser.com>
2018-11-23 10:31:32 +0900

    Web wevice project

--- services/web/seeds/projects.js
+++ services/web/seeds/projects.js
@@ -1,13 +1,11 @@
+const projects = require('./data/projects')
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('projects').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('projects').insert([
-        {id: 1, name: 'rowValue1'},
-        {id: 2, name: 'rowValue2'},
-        {id: 3, name: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('projects')
+        .del()
+        .then(function () {
+            // Inserts seed entries
+            return knex('projects').insert(projects)
+        })
+}
