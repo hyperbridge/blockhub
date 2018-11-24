247411e885b3d59f816d7dd319377d2c37e07a70
Eric Muyser <eric@muyser.com>
2018-11-24 10:49:07 +0900

    Web service

--- services/web/src/api/ping.ts
+++ services/web/src/api/ping.ts
@@ -1,14 +1,14 @@
-const {
-    name,
-    version,
-    description
-} = require('../../../package.json')
+// const {
+//     name,
+//     version,
+//     description
+// } = require('../../../package.json')
 
 export default (_, res) => {
     res.json({
-        name,
-        description,
-        version,
+        name: 'test',
+        description: 'test',
+        version: '0.1',
         uptime: process.uptime()
     })
 }
