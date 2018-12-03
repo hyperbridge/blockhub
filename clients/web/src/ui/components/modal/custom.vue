83a7a64b87a876f19143fac626755bd559bd30d6
soxepo <bogdanvin@gmail.com>
2018-12-03 13:06:11 +0200

    modal bg size fix

--- clients/web/src/ui/components/modal/custom.vue
+++ clients/web/src/ui/components/modal/custom.vue
@@ -42,8 +42,8 @@
         left: 0;
         top: 0;
         z-index: 99;
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         background: rgba(33, 33, 51, 0.9);
         animation: opacity .5s ease;
         display: flex;
