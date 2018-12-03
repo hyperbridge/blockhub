54d50fa9e93e8c945ecde9adceeff159e36f61be
soxepo <bogdanvin@gmail.com>
2018-12-03 20:49:16 +0200

    sidebar slot

--- clients/web/src/ui/components/chat-new/base.vue
+++ clients/web/src/ui/components/chat-new/base.vue
@@ -3,42 +3,8 @@
         <div class="chat-shortcuts">
             <c-shortcut-sidebar :items="shortcuts" />
         </div>
-        <div class="chat__group-info">
-            <div class="chat__group-head">
-                <div class="group-name h5">
-                    General
-                    <i class="fas fa-plus"></i>
-                </div>
-                <div>
-                    12 of 450 online
-                </div>
-                <div class="group-status">
-                    MOTD: Proin ut purus suscipit, bibendum eros
-                </div>
-            </div>
-            <div class="chat__channel-list">
-                <div class="title h6 text-uppercase font-weight-bold d-flex justify-content-between">
-                    Channel
-                    <i class="fas fa-plus"></i>
-                </div>
-                <ul>
-                    <li class="active">
-                        General
-                    </li>
-                    <li>
-                        Trash
-                    </li>
-                    <li>
-                        Only Playload
-                    </li>
-                    <li>
-                        Other staff
-                    </li>
-                    <li>
-                        Help
-                    </li>
-                </ul>
-            </div>
+        <div class="chat__sidebar" v-if="$slots.sidebar">
+            <slot name="sidebar" />
         </div>
         <div class="chat-main">
             <div class="chat-main__header">
@@ -64,18 +30,14 @@
 <script>
     export default {
         props:{
-            shortcuts: [ Array, Object ]
+            shortcuts: [ Array, Object ],
+            showShortcuts: {
+                type: Boolean,
+                default: true
+            }
         },
         components:{
             'c-shortcut-sidebar': (resolve) => require(['@/ui/components/shortcut-sidebar'], resolve)
-        },
-        data(){
-            return{
-                showShortcuts: true,
-                shortcuts:[
-
-                ]
-            }
         }
     }
 </script>
@@ -96,30 +58,12 @@
         display: flex;
         flex-direction: column;
     }
-    .chat__group-info{
+    .chat__sidebar{
         background: #272745;
         flex: 0 0 200px;
         padding: 15px;
         color: #fff;
         border-left: 1px solid rgba(255, 255, 255, .1);
-    }
-    .chat__group-head{
-        color: #fff;
-        padding-bottom: 15px;
-        margin-bottom: 20px;
-        border-bottom: 1px solid rgba(255, 255, 255, .1);
-        .group-name{
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            font-weight: bold;
-            margin-bottom: 0;
-        }
-        .group-status{
-            margin: 10px 0 0;
-            font-size: 12px;
-            line-height: 16px;
-        }
     }
     .chat-main{
         padding: 15px;
