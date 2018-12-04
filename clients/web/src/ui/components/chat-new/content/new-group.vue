27f496f0833f209f7e4adb23203164e983052fb3
soxepo <bogdanvin@gmail.com>
2018-12-04 22:16:25 +0200

    add/join group component - add tabs

--- clients/web/src/ui/components/chat-new/content/new-group.vue
+++ clients/web/src/ui/components/chat-new/content/new-group.vue
@@ -3,50 +3,53 @@
         <a href="#" class="close-btn">
             <i class="fas fa-times"></i>
         </a>
-        <div class="new-group__join-group">
-            <div class="h5 text-center w-100">
-                Join to the group
-            </div>
-            <div class="form-group invert w-100">
-                <label>Enter group invitation link or code</label>
-                <input type="text" class="form-control" placeholder="Enter link or code"
-                       name="group_name" >
-            </div>
-            <c-button status="opacity-info">
-                Join
-            </c-button>
-        </div>
-        <div class="h4 my-4">
-            or
-        </div>
-        <div class="new-group__create-group">
-            <div class="h5 text-center w-100">
-                Create new group
-            </div>
-            <div class="create-group__avatar">
-                <img src="https://cdn1.iconfinder.com/data/icons/people-std/512/customers-512.png" />
-                <c-button status="plain">
-                    Select
+        <div class="new-group__wrapper">
+            <div class="new-group__nav">
+                <c-button status="plain" size="lg" tag="button" :class="{ 'active': create_new }" @click="selectNew">
+                    Create New
+                </c-button>
+                <c-button status="plain" size="lg" tag="button" :class="{ 'active': join }" @click="selectJoin">
+                    Join Group
                 </c-button>
             </div>
-            <div class="create-group__info">
+            <div class="new-group__join-group" v-if="join">
                 <div class="form-group invert w-100">
-                    <label>Group name</label>
-                    <input type="text" class="form-control" placeholder="Enter a name for your group"
+                    <label>Enter group invitation link or code</label>
+                    <input type="text" class="form-control" placeholder="Enter link or code"
                            name="group_name" >
                 </div>
-                <div class="form-group invert w-100">
-                    <label>Description</label>
-                    <textarea class="form-control" placeholder="Enter a name for your group"
-                              name="group_description"></textarea>
+                <div class="text-right w-100">
+                    <c-button status="opacity-info">
+                        Join
+                    </c-button>
                 </div>
-                <div class="w-100 text-center margin-top-20">
-                    <c-button status="opacity-success" class="mx-2">
-                        Create
+            </div>
+            <div class="new-group__create-group" v-if="create_new">
+                <div class="create-group__avatar">
+                    <img src="https://cdn1.iconfinder.com/data/icons/people-std/512/customers-512.png" />
+                    <c-button status="plain">
+                        Select
                     </c-button>
-                    <c-button status="opacity-warning" class="mx-2">
-                        Cancel
-                    </c-button>
+                </div>
+                <div class="create-group__info">
+                    <div class="form-group invert w-100">
+                        <label>Group name</label>
+                        <input type="text" class="form-control" placeholder="Enter a name for your group"
+                               name="group_name" >
+                    </div>
+                    <div class="form-group invert w-100">
+                        <label>Description</label>
+                        <textarea class="form-control" placeholder="Enter a name for your group"
+                                  name="group_description"></textarea>
+                    </div>
+                    <div class="w-100 text-right margin-top-20">
+                        <c-button status="opacity-success" class="ml-2">
+                            Create
+                        </c-button>
+                        <c-button status="opacity-warning" class="ml-2">
+                            Cancel
+                        </c-button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -55,6 +58,22 @@
 
 <script>
     export default {
+        data(){
+            return{
+                join: false,
+                create_new: true
+            }
+        },
+        methods:{
+            selectJoin(){
+                this.join = true;
+                this.create_new = false;
+            },
+            selectNew(){
+                this.join = false;
+                this.create_new = true;
+            }
+        }
     }
 </script>
 
@@ -79,12 +98,29 @@
         top: 15px;
         right: 15px;
     }
+    .new-group__wrapper{
+        width: 90%;
+        max-width: 600px;
+    }
+    .new-group__nav{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 40px;
+        .c-btn{
+            padding: 5px 0;
+            border-radius: 0;
+            border-bottom: 2px solid transparent;
+            &.active{
+                border-color: #fff;
+            }
+        }
+    }
     .new-group__create-group,
     .new-group__join-group{
         display: flex;
         flex-wrap: wrap;
         width: 100%;
-        max-width: 600px;
     }
     .create-group__avatar{
         text-align: center;
