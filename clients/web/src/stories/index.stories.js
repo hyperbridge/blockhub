bba733231de195a1fd2812e2e167a9f663ac8366
Eric Muyser <eric@muyser.com>
2018-12-11 16:00:44 +0900

    Fix broken renames (be more careful Eric)

--- clients/web/src/stories/index.stories.js
+++ clients/web/src/stories/index.stories.js
@@ -2993,7 +2993,7 @@ storiesOf('User Card', module)
             <div class="col-4">
                 <c-user-card
                     :user="user"
-                    @updateprofile="(prop, val) => user[prop] = val"
+                    @updateProfile="(prop, val) => user[prop] = val"
                 />
             </div>
             <div class="col-4">
