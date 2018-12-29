ac61e43f0004ce1f92829d043864aec01feb65d2
soxepo <bogdanvin@gmail.com>
2018-12-29 23:23:24 +0200

    image-overlay change z-index and gradient

--- clients/web/src/ui/components/image-overlay/index.vue
+++ clients/web/src/ui/components/image-overlay/index.vue
@@ -30,12 +30,12 @@
         right: 0;
         padding: 20px;
         min-height: 70%;
-        background: -webkit-linear-gradient(bottom, rgba(28, 32, 50, 1) 0%, rgba(28, 32, 50, 0.75) 40%, rgba(28, 32, 50, 0) 100%);
-        z-index: 20;
+        background: -webkit-linear-gradient(bottom, rgba(28, 32, 50, 1) 8%, rgba(28, 32, 50, 0.75) 50%, rgba(28, 32, 50, 0) 100%);
+        z-index: 10;
     }
     .image-overlay__title{
         font-weight: bolder;
-        font-size: 20px;
+        font-size: 23px;
         margin-bottom: 10px;
     }
     .image-overlay__subtitle{
