2d49bd48d75fbf4756658044deba6d21efd0741e
soxepo <bogdanvin@gmail.com>
2019-01-28 02:00:52 +0200

    Features screen

--- clients/web/src/ui/screens/features/index.vue
+++ clients/web/src/ui/screens/features/index.vue
@@ -1,20 +1,142 @@
 <template>
     <c-layout :showLeftPanel="false" :showRightPanel="false">
-        <div class="row justify-content-center">
-            <div class="col-12 col-md-10 col-lg-8 col-xl-6 text-center">
-                <div class="display-5 mb-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies nisi ante.
-                </div>
-                <div class="h4 font-weight-light">
-                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
-                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
-                    porta sodales. Mauris pharetra enim quis lacus ullamcorper, quis posuere ante luctus.
-                </div>
-                <div class="h1 font-weight-bold mt-5">
-                    Quisque non faucibus nisl.
+        <section class="section_1 px-3">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-10 col-lg-8 col-xl-6 text-center">
+                    <div class="display-5 mb-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies nisi ante.
+                    </div>
+                    <div class="h4 font-weight-light">
+                        Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                        arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                        porta sodales. Mauris pharetra enim quis lacus ullamcorper, quis posuere ante luctus.
+                    </div>
+                    <div class="h1 font-weight-bold mt-5">
+                        Quisque non faucibus nisl.
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+        <section class="section_2 px-3">
+            <div class="row align-items-end justify-content-end h-100">
+                <div class="col-4">
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                </div>
+            </div>
+        </section>
+        <section class="section_3 px-3">
+            <div class="row align-items-center h-100">
+                <div class="col-4 padding-bottom-50 padding-top-50">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                </div>
+            </div>
+        </section>
+        <section class="section_4 px-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-md-10 col-lg-7 text-center">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                    <img src="https://bitsrc.io/dist/f0f25596ce442161664bc9daf5cec798.png" class="img-fluid mt-5">
+                </div>
+            </div>
+        </section>
+        <section class="section_4 px-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-lg-4">
+                    <img src="https://bitsrc.io/dist/cd3bd04cd27f1f9a64f88c56bfe0e7cd.jpg" class="img-fluid">
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                </div>
+            </div>
+        </section>
+        <section class="section_6 px-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-md-10 col-lg-7 text-center">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                </div>
+            </div>
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-md-5 col-lg-5">
+                    <img src="https://bitsrc.io/dist/1c77f739c4d5bb4d9100ef4cb29bbaec.png" class="img-fluid mt-5">
+                </div>
+                <div class="col-12 col-md-5 col-lg-3">
+                    <img src="https://bitsrc.io/dist/bec34bb5f85ebf617489056daee85dda.png" class="img-fluid mt-5">
+                </div>
+            </div>
+        </section>
+        <section class="section_7 px-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-lg-4">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                </div>
+                <div class="col-12 col-lg-4 text-center">
+                    <img src="https://bitsrc.io/dist/af8fb298d7808088a37469d9268ac773.jpg" class="img-fluid">
+                </div>
+            </div>
+        </section>
+        <section class="section_8 px-3">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 col-md-10 col-lg-7 text-center">
+                    <div class="h1 font-weight-bold mb-4">
+                        Quisque non faucibus nisl.
+                    </div>
+                    Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                    arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque. Mauris sodales arcu nec lacus
+                    porta sodales.
+                    <img src="https://bitsrc.io/dist/d03b3285fa74887bb77040bc6996a0a5.png" class="img-fluid mt-5">
+                </div>
+            </div>
+        </section>
+        <section class="section_9">
+            <div class="row justify-content-around">
+                <div class="col-12 mb-4">
+                    <div class="h1 font-weight-bold text-center mb-5">
+                        Quisque non faucibus nisl.
+                    </div>
+                </div>
+                <div class="col-12 col-md-5 col-lg-4">
+                    <img src="https://bitsrc.io/dist/82275db83fcdb77d27216c49ed678f32.jpg" width="110" class="d-block mx-auto mb-5" />
+                    <p>
+                        Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                        arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque.
+                    </p>
+                </div>
+                <div class="col-12 col-md-5 col-lg-4">
+                    <img src="https://bitsrc.io/dist/29fa571ba4124c70463d07f5740a646a.jpg" width="110" class="d-block mx-auto mb-5" />
+                    <p>
+                        Etiam a arcu ac justo accumsan molestie nec vel nunc. Praesent vitae hendrerit diam, id faucibus
+                        arcu. Nulla vel lacus vitae ligula pretium consequat ac et neque.
+                    </p>
+                </div>
+            </div>
+        </section>
     </c-layout>
 </template>
 
@@ -23,5 +145,56 @@
         font-weight: 300;
         font-size: 36px;
         line-height: normal;
+    }
+    .section_1{
+        background: #fff;
+        color: #000;
+        padding: 10vh 0;
+        font-size: 18px;
+        line-height: 32px;
+    }
+    .section_2{
+        background: url('https://bitsrc.io/dist/4bd02afe5eb7c8c0322f385b08e389af.jpg') #fff no-repeat top center;
+        background-size: auto 120%;
+        height: 40vw;
+        padding: 10vh 0;
+        color: #000;
+        font-size: 18px;
+        line-height: 32px;
+    }
+    .section_3{
+        background: url("https://bitsrc.io/dist/26f9fc664341d163c3ef107c6602d77e.png") #fff no-repeat;
+        background-position: center right;
+        background-size: 60% auto;
+        color: #000;
+        padding: 15vh 0;
+        font-size: 18px;
+        line-height: 32px;
+        min-height: 40vh;
+        img{
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 15px;
+            height: auto;
+        }
+    }
+    .section_4,
+    .section_5,
+    .section_6,
+    .section_7,
+    .section_9{
+        background: #fff;
+        color: #000;
+        padding: 7vh 0;
+        font-size: 18px;
+        line-height: 32px;
+    }
+    .section_8{
+        background: #363139;
+        color: #fff;
+        padding: 10vh 0;
+        font-size: 18px;
+        line-height: 32px;
     }
 </style>
