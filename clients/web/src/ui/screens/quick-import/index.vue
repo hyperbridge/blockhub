2f26c5f51ec629a51ee6f3be3a58226dba9fdf4d
soxepo <bogdanvin@gmail.com>
2019-02-02 16:10:47 +0200

    Quick import screen

--- clients/web/src/ui/screens/quick-import/index.vue
+++ clients/web/src/ui/screens/quick-import/index.vue
@@ -1,16 +1,11 @@
 <template>
     <c-layout navigationKey="store">
         <div class="row">
-            <div class="col-12">
-                <div class="h2 text-center margin-bottom-30">
-                    Add projects
-                </div>
-            </div>
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-6" v-if="!quickImport && !integrateLocally">
                 <c-block title="Quick import" bgGradient noGutter onlyContentBg>
                     <div class="text-center pb-4">
                         <p>Sed posuere varius ex id ullamcorper. Donec et aliquet mi, in tincidunt massa. Maecenas ornare, elit nec condimentum semper</p>
-                        <c-button status="second-info" size="md" class="mt-3 px-5">
+                        <c-button status="second-info" size="md" class="mt-3 px-5" @click=" quickImport = true">
                             Continue
                         </c-button>
                         <small class="text-muted d-block text-center mt-2">
@@ -19,11 +14,11 @@
                     </div>
                 </c-block>
             </div>
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-6" v-if="!quickImport && !integrateLocally">
                 <c-block title="Integrate locally" bgGradient noGutter onlyContentBg>
                     <div class="text-center pb-4">
                         <p>Sed posuere varius ex id ullamcorper. Donec et aliquet mi, in tincidunt massa. Maecenas ornare, elit nec condimentum semper</p>
-                        <c-button status="second-info" size="md" class="mt-3 px-5">
+                        <c-button status="second-info" size="md" class="mt-3 px-5" @click=" integrateLocally = true">
                             View Guide
                         </c-button>
                         <small class="text-success d-block text-center mt-2">
@@ -32,16 +27,52 @@
                     </div>
                 </c-block>
             </div>
+            <div class="col-12" v-if="signedIn && quickImport">
+                <c-block title="Quick import" bgGradient noGutter onlyContentBg>
+                    <p class="mb-3">
+                        Automatically analyze from code host for easy initial result.
+                    </p>
+                    <c-icon-block icon="gem" size="md">
+                        Aliquam erat volutpat
+                    </c-icon-block>
+                </c-block>
+            </div>
+            <div class="col-12" v-if="!signedIn && quickImport">
+                <div class="h6">
+                    You are not signed in. Please
+                    <c-button status="plain" @click="$store.commit('application/activateModal', 'login')">
+                        Sign In
+                    </c-button>
+                    to continue.
+                </div>
+            </div>
         </div>
     </c-layout>
 </template>
 
 <script>
     export default {
-
+        components: {
+            'c-icon-block': (resolve) => require(['@/ui/components/block/with-icon'], resolve)
+        },
+        data(){
+            return{
+                quickImport: false,
+                integrateLocally: false
+            }
+        },
+        computed: {
+            signedIn() {
+                return this.$store.state.application.signedIn
+            },
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .block-icon__icon{
+        border: 1px solid rgba( 255, 255, 255, .2);
+        padding: 15px;
+        border-radius: 5px;
+    }
 </style>
