b6e6aeddc3d960d49ddf484061765b2a1c698406
soxepo <bogdanvin@gmail.com>
2019-02-08 20:16:11 +0200

    test screen

---
+++ clients/web/src/ui/screens/video/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <c-layout>
+        <div class="col-6">
+            <c-video src="https://www.w3schools.com/tags/mov_bbb.mp4" :controls="true" />
+        </div>
+    </c-layout>
+</template>
+
+<script>
+    export default {
+        components: {
+            'c-video': (resolve) => require(['@/ui/components/video'], resolve)
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
