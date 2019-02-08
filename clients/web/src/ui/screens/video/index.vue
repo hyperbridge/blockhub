dfff8724a6adf6fc0e396b5be56a36dd8d7c0e48
soxepo <bogdanvin@gmail.com>
2019-02-08 21:18:02 +0200

    minor changes

--- clients/web/src/ui/screens/video/index.vue
+++ clients/web/src/ui/screens/video/index.vue
@@ -1,7 +1,7 @@
 <template>
     <c-layout>
-        <div class="col-6">
-            <c-video src="https://www.w3schools.com/tags/mov_bbb.mp4" :controls="true" />
+        <div class="col-12">
+            <c-video src="https://www.w3schools.com/tags/mov_bbb.mp4" width="100%" :controls="true" />
         </div>
     </c-layout>
 </template>
@@ -14,6 +14,3 @@
     }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
