1dc83e0cec56d7709142f8a59b3128752f29d7ac
soxepo <bogdanvin@gmail.com>
2019-04-07 18:32:48 +0300

    payment screen

--- clients/web/src/ui/screens/payment/index.vue
+++ clients/web/src/ui/screens/payment/index.vue
@@ -3,11 +3,53 @@
         <div class="row">
             <div class="col-12 col-md-8">
                 <c-block title="Payment method">
-                    <div class="h4"></div>
-                    Choose payment system
+                    <div class="form-group">
+                        <label>
+                            Choose payment system
+                        </label>
+                        <c-multiselect v-model="paymentSystem"
+                                       class="dark-mode"
+                                       label="label"
+                                       :options="paymentSystems">
+                        </c-multiselect>
+                    </div>
                 </c-block>
             </div>
-            <div class="col-12 col-md-4"></div>
+            <div class="col-12 col-md-4">
+                <c-block title="Payment methods">
+                    Choose Methods
+                </c-block>
+            </div>
         </div>
     </c-layout>
 </template>
+
+<script>
+    import 'vue-multiselect/dist/vue-multiselect.min.css'
+
+    export default {
+        components: {
+            'c-multiselect': (resolve) => require(['vue-multiselect'], resolve),
+        },
+        data() {
+            return {
+                paymentSystem: null,
+                paymentSystems:[
+                    {
+                        label: 'Vise',
+                        key: 'visa'
+                    },
+                    {
+                        label: 'MasterCard',
+                        key: 'mastercard'
+                    },
+                    {
+                        label: 'Bank USA',
+                        key: 'bank-usa'
+                    },
+                ]
+            }
+        },
+    }
+</script>
+
