9b4bc25e0840679b1244ac20e94f74e4b378382d
soxepo <bogdanvin@gmail.com>
2019-05-24 18:15:03 +0300

    contributor-box component

--- clients/web/src/ui/components/contributor-box/index.vue
+++ clients/web/src/ui/components/contributor-box/index.vue
@@ -3,11 +3,30 @@
         <div class="c-contributor-box__title">
             Become a product Hunt contributor
         </div>
+        <div class="c-contributor-box__description">
+            <div class="text-white margin-bottom-15">
+                Becoming a Product Hunt contributor allows you to hunt products.
+            </div>
+            <div class="margin-bottom-5">
+                4 steps left!
+            </div>
+            <c-progress-bar :values="{ reached: '1', goal: '4'}"/>
+            <div class="text-right margin-top-10">
+                <c-button status="second-info">
+                    Learn More
+                </c-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: {},
+        components: {
+            'c-progress-bar': (resolve) => require(['@/ui/components/progress-bar'], resolve)
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -15,6 +34,20 @@
         background: #26263d;
         padding: 25px;
         border-radius: 5px;
+        display: inline-flex;
+        max-width: 100%;
+        color: #fff;
+        flex-direction: column;
+    }
+    .c-contributor-box__title{
+        padding-bottom: 15px;
+        margin-bottom: 15px;
+        border-bottom: 1px solid rgba(255, 255, 255, .1);
+        color: #fff;
+        font-size: 22px;
+        font-weight: 500;
+        display: flex;
+        width: 100%;
     }
 </style>
 
