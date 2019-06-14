285a339c444afb2bf5d57fe49a2983ec45e4c62d
soxepo <bogdanvin@gmail.com>
2019-06-14 20:14:22 +0300

    Identity (badges page)

--- clients/web/src/ui/screens/identity/index.vue
+++ clients/web/src/ui/screens/identity/index.vue
@@ -12,17 +12,19 @@
                 </div>
                 <div>
                     <ul class="badge-list">
-                        <li class="badge-list__item" v-for="i in 6">
+                        <li class="badge-list__item"
+                            :class="{'badge-list__item--disabled': badge.disabled}"
+                            v-for="(badge, idx) in badges"
+                            :key="`badge-${idx}`">
                             <div class="badge-list__item-icon">
-                                <i class="fas fa-shield-alt"></i>
+                                <i :class="`fas ${badge.icon}`"></i>
                             </div>
                             <div class="badge-list__item-text">
-                                <div class="h5">
-                                    Duis feugiat dictum porta
+                                <div class="h5 font-weight-bold">
+                                    {{ badge.title }}
                                 </div>
                                 <p>
-                                    Sed suscipit elementum accumsan. Morbi fringilla sem
-                                    elit, ac elementum tellus egestas quis.
+                                    {{ badge.description }}
                                 </p>
                             </div>
                         </li>
@@ -34,7 +36,71 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: {
+            id: String,
+        },
+        data(){
+            return{
+                badges: [
+                    {
+                        icon: 'fa-shield-alt',
+                        title: 'Duis feugiat dictum porta',
+                        description: 'Sed suscipit elementum accumsan. Morbi fringilla sem elit, ac elementum tellus egestas quis.',
+                        disabled: false
+                    },
+                    {
+                        icon: 'fa-leaf',
+                        title: 'Nullam in libero',
+                        description: 'Quisque ornare nisl a vestibulum feugiat. Proin nisl lorem, lacinia non mi nec, cursus sollicitudin nunc.',
+                        disabled: true
+                    },
+                    {
+                        icon: 'fa-crown',
+                        title: 'Proin ac tellus tempus',
+                        description: 'Nunc maximus nunc est. Nullam vulputate nisi eros, eget porta ex porta vitae.',
+                        disabled: true
+                    },
+                    {
+                        icon: 'fa-cannabis',
+                        title: 'Suspendisse ac lorem rutrum',
+                        description: 'Vivamus convallis, ligula id tincidunt egestas, nunc odio ultrices leo, in luctus nisi magna ut quam.',
+                        disabled: false
+                    },
+                    {
+                        icon: 'fa-certificate',
+                        title: 'Class aptent taciti sociosqu ad',
+                        description: 'Aliquam fringilla aliquet pellentesque. Class aptent taciti per conubia nostra, per inceptos himenaeos.',
+                        disabled: false
+                    },
+                    {
+                        icon: 'fa-bolt',
+                        title: 'Sed vestibulum molestie',
+                        description: 'Nulla in nibh nec urna laoreet gravida vel suscipit nulla',
+                        disabled: false
+                    },
+                    {
+                        icon: 'fa-award',
+                        title: 'Ut lacinia nec ante vitae cursus',
+                        description: 'Sed varius nisl vitae leo rhoncus, accumsan tincidunt orci sagittis. Donec dictum, lectus at accumsan rutrum.',
+                        disabled: true
+                    },
+                    {
+                        icon: 'fa-bell',
+                        title: 'Suspendisse a varius nisi',
+                        description: 'Quisque ornare nisl a vestibulum feugiat. Proin nisl lorem, lacinia non mi nec, cursus sollicitudin nunc.',
+                        disabled: true
+                    },
+                    {
+                        icon: 'fa-cookie',
+                        title: 'Morbi lobortis laoreet neque',
+                        description: 'Vestibulum ornare porttitor libero. Morbi lobortis laoreet neque a rhoncus.',
+                        disabled: true
+                    },
+                ]
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
@@ -54,6 +120,22 @@
         display: flex;
         align-items: flex-start;
         justify-content: space-between;
+        &--disabled{
+            .badge-list__item-icon{
+                &:after{
+                    content: "";
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    bottom: 0;
+                    left: 0;
+                    background: rgba(0, 0, 0, .6);
+                }
+            }
+            .badge-list__item-text{
+                opacity: .5;
+            }
+        }
     }
     .badge-list__item-icon{
         margin-right: 20px;
@@ -67,6 +149,8 @@
         border-radius: 5px;
         background: rgba(255, 255, 255, .05);
         border: 1px solid rgba(0, 0, 0, .1);
+        position: relative;
+        overflow: hidden;
     }
     .badge-list__item-text{
         width: calc( 100% - 60px )
