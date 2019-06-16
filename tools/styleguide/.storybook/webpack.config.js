df71e4f8176a309c80f14d9f1aaf825d1d1808dd
Eric Muyser <eric@muyser.com>
2019-06-16 15:30:25 +0900

    Fix styleguide

--- tools/styleguide/.storybook/webpack.config.js
+++ tools/styleguide/.storybook/webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = (storybookBaseConfig, configType, defaultConfig) => {
     defaultConfig.module.rules.push({
         test: /\.scss$/,
-        loaders: ["vue-style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../../../clients/web-client-new")
+        loaders: ['vue-style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../../../clients/web')
     })
 
-    defaultConfig.resolve.alias['@'] = path.resolve(__dirname, "../../../clients/web-client-new/ui")
+    defaultConfig.resolve.alias['@'] = path.resolve(__dirname, '../../../clients/web/ui')
 
-    defaultConfig.resolve.alias['~'] = path.resolve(__dirname, "../../../clients/web-client-new/ui")
+    defaultConfig.resolve.alias['~'] = path.resolve(__dirname, '../../../clients/web/ui')
 
     defaultConfig.node = {
         fs: 'empty'
