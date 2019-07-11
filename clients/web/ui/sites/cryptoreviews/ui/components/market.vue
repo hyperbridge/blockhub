5e8b93fb8d77853a06683ce2492913b492dd73fc
Eric Muyser <eric@muyser.com>
2019-07-11 19:09:19 +0900

    [WIP] Generic routing

--- clients/web/ui/sites/cryptoreviews/ui/components/market.vue
+++ clients/web/ui/sites/cryptoreviews/ui/components/market.vue
@@ -4,16 +4,18 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="heading-title mb-0">
-                        <h3 class="title tw-5 mb-20"><a href="#market">Market</a></h3>
+                        <h3 class="title tw-5 mb-20">
+                            <a href="#market">Market</a>
+                        </h3>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="btcwdgt-chart"></div>
+                    <div class="btcwdgt-chart" />
                 </div>
                 <div class="col-lg-6 r-mt-40">
-                    <div class="btcwdgt-news"></div>
+                    <div class="btcwdgt-news" />
                 </div>
             </div>
         </div>
@@ -22,7 +24,7 @@
 
 
 <script>
-    export default {
-        props: ['list']
-    }
+export default {
+    props: ['list']
+}
 </script>
