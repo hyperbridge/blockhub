37d3be49b43515c5b4116bba4b9c3c1846f1f309
Eric Muyser <eric@muyser.com>
2019-07-22 21:43:19 +0900

    Broken page haikus

--- clients/web/ui/components/broken-page/index.vue
+++ clients/web/ui/components/broken-page/index.vue
@@ -4,17 +4,30 @@
             class="col-12"
             style="text-align: center">
             <br><br><br><br><br><br><br><br><br>
-            <h1 v-if="isError.statusCode === 404">
-                <strong>GG.</strong> Page not found.
-            </h1>
-            <h1 v-else>
-                <strong>GG.</strong> You broke it.
-            </h1>
-            <h3>Maybe there was an error in the spacetime continuum. <br>You better get home quick.</h3>
+            <div v-if="!haiku">
+                <h1 v-if="isError.statusCode === 404">
+                    <strong>GG.</strong> Page not found.
+                </h1>
+                <h1 v-else>
+                    <strong>GG.</strong> You broke it.
+                </h1>
+                <h3>Maybe there was an error in the spacetime continuum. <br>You better get home quick.</h3>
+                <br><br>
+                <p class="error">
+                    Error status: {{ isError.statusCode }}<br>
+                    Error message: {{ isError.message }}
+                </p>
+            </div>
+            <div v-else class="haiku">
+                <h1>{{ haiku[0] }}</h1>
+                <h3>{{ haiku[1] }}</h3>
+                <h3>{{ haiku[2] }}</h3>
+                <p class="error">
+                    Error status: {{ isError.statusCode }}<br>
+                    Error message: {{ isError.message }}
+                </p>
+            </div>
             <br>
-            <p style="font-style: italic">
-                Error message: {{ isError.message }}
-            </p>
             <br><br>
             <c-button
                 class="c-button--xl"
@@ -40,6 +53,44 @@ export default {
             if (process.client) {
                 this.$('body').removeClass('screen--not-found')
             }
+        }
+    },
+    data() {
+        let haikus = {
+            general: [
+                ['Page you are seeking', 'From your path it is fleeing', 'Its winter has come.'],
+                ['The web page you seek', 'Lies beyond our perception', 'But others await.'],
+                ['Cables have been cut', 'Southwest of Northeast somewhere', 'We are not amused.'],
+            ],
+            404: [
+                ['Not a pretty sight', 'When the web dies screaming loud', 'The page is not found.'],
+                ['With searching comes loss', 'And the presence of absence:', 'The page is not found.'],
+                ['Mourning and sorrow', 'Page not with us now', 'Lost in 404.'],
+                ['Rather than a beep', 'Or a rude error message,', `These words: 'Page not found.'`],
+                ['These three are certain:', 'Death, taxes, and page not found.', 'You, victim of one.'],
+                ['Click exciting link', 'Silicon hold you back', '404 not found.'],
+            ],
+            500: [
+                ['Error reduces', 'Your expensive computer', 'To a simple stone.'],
+                ['Aborted effort:', 'The site, passed this veil of tears.', 'You ask way too much.'],
+                ['Chaos reigns within.', 'Reflect, repent, and retry.', 'Server shall return.'],
+                [`Won't you please observe`, 'A brief moment of silence', 'For the dead page'],
+                ['To have no errors', 'Would be life without meaning', 'No struggle, no joy'],
+                ['First snow, then silence.', 'This expensive server dies', 'So beautifully.'],
+                ['Silicon shudders', 'The page is down for the count', 'One big knockout punch.'],
+                ['Errors have occurred.', `We won't tell you where or why.`, 'Lazy programmers.'],
+                ['Yesterday it worked', 'Today it is not working', 'The web is like that.'],
+                ['The code was willing', 'It considered your request,', 'But the proposal was weak.']
+            ],
+            700: [
+                [`Server's poor response`, 'Not quick enough for browser.', 'Timed out, plum blossom.']
+            ]
+        }
+
+        haikus = [...haikus['general'], ...(haikus[this.isError.statusCode] || [])]
+
+        return {
+            haiku: haikus[Math.floor(Math.random() * haikus.length)]
         }
     },
     created() {
@@ -99,6 +150,17 @@ export default {
         }
         .quick-launch {
             display: none;
+        }
+        .haiku {
+            display: inline-block;
+            text-align: left;
+            h3 {
+                margin-left: 30px;
+            }
+        }
+        .error {
+            margin-left: 30px;
+            font-style: italic;
         }
     }
 </style>
