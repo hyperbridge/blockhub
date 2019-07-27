64b8ada8ce66d86a92c32abeb2b002afbd19ab7b
Eric Muyser <eric@muyser.com>
2019-07-27 19:31:09 +0900

    Realm subpages

---
+++ clients/web/ui/pages/realm/_id/community.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            Community
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['realm']
+}
+</script>
---
+++ clients/web/ui/pages/realm/_id/games.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            Games
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['realm']
+}
+</script>
